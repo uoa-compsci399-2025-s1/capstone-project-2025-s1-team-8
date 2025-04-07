0884a49d7b3ea973e0ef7881f2ebac8eb25542e9
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-08 11:43:25 +1200

    Updated formatting with prettier

--- src/collections/Form.ts
+++ src/collections/Form.ts
@@ -1,48 +1,47 @@
 import type { CollectionConfig } from 'payload'
 
 export const Form: CollectionConfig = {
-    slug: 'form',
-    access: {},
-    fields: [
-      {
-        name: 'name',
-        type: 'text',
-        required: true,
-      },
-      {
-        name: 'clients',
-        type: 'relationship',
-        relationTo: 'user',
-        hasMany: true,
-        validate: async (val, args) => {
-          if (!val || val.length === 0) return true;
-      
-          const clientIDs = Array.isArray(val) ? val : [val];
-      
-          const users = await args.req.payload.find({
-            collection: 'user',
-            where: {
-              id: {
-                in: clientIDs,
-              },
+  slug: 'form',
+  access: {},
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'clients',
+      type: 'relationship',
+      relationTo: 'user',
+      hasMany: true,
+      validate: async (val, args) => {
+        if (!val || val.length === 0) return true
+
+        const clientIDs = Array.isArray(val) ? val : [val]
+
+        const users = await args.req.payload.find({
+          collection: 'user',
+          where: {
+            id: {
+              in: clientIDs,
             },
-          });
-      
-          const nonClients = users.docs.filter((user) => user.role !== 'client');
-      
-          if (nonClients.length > 0) {
-            const names = nonClients.map((u) => `${u.firstName} ${u.lastName}`).join(', ');
-            return `The following users are not clients: ${names}`;
-          }
-          return true;
-        },
+          },
+        })
+
+        const nonClients = users.docs.filter((user) => user.role !== 'client')
+
+        if (nonClients.length > 0) {
+          const names = nonClients.map((u) => `${u.firstName} ${u.lastName}`).join(', ')
+          return `The following users are not clients: ${names}`
+        }
+        return true
       },
-      {
-        name: 'questions',
-        type: 'relationship',
-        relationTo: 'formQuestion',
-        hasMany: true,
-      },
-    ],
-  }
-  
+    },
+    {
+      name: 'questions',
+      type: 'relationship',
+      relationTo: 'formQuestion',
+      hasMany: true,
+    },
+  ],
+}
