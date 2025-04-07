928e198cec61e946b06aa30cdaca09e7c07dd839
Enshean Lin <elin277@aucklanduni.ac.nz>
2025-04-07 13:48:59 +1200

    codegen added in ci-pipeline (#38)
    * codegen added in ci-pipeline
    
    * run prettier write
    
    * reverted payload-types.ts, add to prettierignore, add to verify in action
    
    * Add vitest + setup + basic example test (#35)
    
    * reformat test file
    
    ---------
    
    Co-authored-by: Sheena Lin <ensheanlin@gmail.com>
    Co-authored-by: Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>

--- tests/abc.test.ts
+++ tests/abc.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 function sum(a: number, b: number): number {
-    return a + b
-    }
+  return a + b
+}
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
