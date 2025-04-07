b691c45854d896e9dd9f4fedf71fed128d481539
Sheena Lin <ensheanlin@gmail.com>
2025-04-07 15:45:38 +1200

    basic User collection created

---
+++ src/collections/User.ts
@@ -0,0 +1,54 @@
+import type { CollectionConfig } from 'payload'
+
+export const User: CollectionConfig = {
+  slug: 'user',
+  auth: true,
+  fields: [
+    {
+      name: 'firstName',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'lastName',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'email',
+      type: 'email',
+      required: true
+    },
+    {
+      name: 'password',
+      // check type for password ??
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'role',
+      type: 'select',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin'
+        },
+        {
+          label: 'User',
+          value: 'user'
+        },
+        {
+          label: 'Student',
+          value: 'student'
+        },
+      ],
+      required: true
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: false
+    },
+  ],
+}
