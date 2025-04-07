59496e4dca3de2dbf0a08ffc232203251b4386f8
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-08 11:42:09 +1200

    Removed id from form collection and added validation for user being client

--- src/collections/Form.ts
+++ src/collections/Form.ts
@@ -5,11 +5,6 @@ export const Form: CollectionConfig = {
     access: {},
     fields: [
       {
-        name: 'id',
-        type: 'text',
-        required: true,
-      },
-      {
         name: 'name',
         type: 'text',
         required: true,
@@ -19,6 +14,28 @@ export const Form: CollectionConfig = {
         type: 'relationship',
         relationTo: 'user',
         hasMany: true,
+        validate: async (val, args) => {
+          if (!val || val.length === 0) return true;
+      
+          const clientIDs = Array.isArray(val) ? val : [val];
+      
+          const users = await args.req.payload.find({
+            collection: 'user',
+            where: {
+              id: {
+                in: clientIDs,
+              },
+            },
+          });
+      
+          const nonClients = users.docs.filter((user) => user.role !== 'client');
+      
+          if (nonClients.length > 0) {
+            const names = nonClients.map((u) => `${u.firstName} ${u.lastName}`).join(', ');
+            return `The following users are not clients: ${names}`;
+          }
+          return true;
+        },
       },
       {
         name: 'questions',
