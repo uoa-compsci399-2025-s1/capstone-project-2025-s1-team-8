28a943d231407b65c1651644a3bc3e64f83c7b85
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-12 21:00:45 +1200

    ran yassify

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 type ButtonColor = {
-    color: 'light' | 'dark';
+  color: 'light' | 'dark'
 }
 
-const Button : React.FC<ButtonColor> = ({color}) => {
-    const lightColor = 'bg-white text-black'
-    const darkColor = 'bg-black text-white'
-    return (
-        <button className={`${color == 'light'? lightColor : darkColor}`}>Click here</button>
-    )
+const Button: React.FC<ButtonColor> = ({ color }) => {
+  const lightColor = 'bg-white text-black'
+  const darkColor = 'bg-black text-white'
+  return <button className={`${color == 'light' ? lightColor : darkColor}`}>Click here</button>
 }
-export default Button;
+export default Button
