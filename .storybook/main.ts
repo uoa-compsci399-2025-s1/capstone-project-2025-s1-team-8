edbc27a95240809390c27f11a97dff3445f0bf5e
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-12 21:05:05 +1200

    more prettier checks

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
@@ -10,6 +10,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
-};
+}
 
-export default config;
+export default config
