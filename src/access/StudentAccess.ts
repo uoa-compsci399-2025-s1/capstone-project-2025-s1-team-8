645f0fccf112c268e2c1535ec3c6de461d783674
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-13 22:22:04 +1200

    Add client and student only access functions as well

---
+++ src/access/StudentAccess.ts
@@ -0,0 +1,8 @@
+import type { Access } from 'payload'
+
+import { checkRole } from './checkRole'
+
+export const admin: Access = ({ req: { user } }): boolean => {
+  if (!user) return false
+  return checkRole(['student'], user)
+}
