5583a43387d064cc6a12ef5fb93f44b4e38724ea
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-14 14:48:10 +1200

    Add tests for checkRole

---
+++ src/access/checkRole.test.ts
@@ -0,0 +1,56 @@
+import {expect, test} from 'vitest'
+import {checkRole} from './checkRole'
+import {User} from '@/payload-types'
+import {Role} from '@/types/RoleTypes'
+
+test("Check for Admin access", () => {
+    const user1: User = {
+        id: "1",
+        role: Role.Admin,
+        firstName: "John",
+        lastName: "Doe",
+        updatedAt: "now",
+        createdAt: "now",
+        email: "johndoe@gmail.com"
+
+    }
+    expect(checkRole([Role.Admin], user1)).toBe(true)
+    expect(checkRole([Role.Client], user1)).toBe(false)
+    expect(checkRole([Role.Admin, Role.Client], user1)).toBe(true)
+    expect(checkRole([Role.Client, Role.Student], user1)).toBe(false)
+})
+
+test("Check for Client access", () => {
+    const user1: User = {
+        id: "1",
+        role: Role.Client,
+        firstName: "John",
+        lastName: "Doe",
+        updatedAt: "now",
+        createdAt: "now",
+        email: "johndoe@gmail.com"
+
+    }
+    expect(checkRole([Role.Client], user1)).toBe(true)
+    expect(checkRole([Role.Admin], user1)).toBe(false)
+    expect(checkRole([Role.Admin, Role.Client], user1)).toBe(true)
+    expect(checkRole([Role.Client, Role.Student], user1)).toBe(true)
+})
+
+test("Check for Student access", () => {
+    const user1: User = {
+        id: "1",
+        role: Role.Student,
+        firstName: "John",
+        lastName: "Doe",
+        updatedAt: "now",
+        createdAt: "now",
+        email: "johndoe@gmail.com"
+
+    }
+    expect(checkRole([Role.Student], user1)).toBe(true)
+    expect(checkRole([Role.Client], user1)).toBe(false)
+    expect(checkRole([Role.Admin, Role.Client], user1)).toBe(false)
+    expect(checkRole([Role.Client, Role.Student], user1)).toBe(true)
+    expect(checkRole([Role.Admin, Role.Student], user1)).toBe(true)
+})
