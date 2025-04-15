ea6a93993f5e985853047ba80eb10d92b9b3ccd8
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-16 11:00:31 +1200

    Added error styling for input fields

--- src/components/Generic/Input/InputField.tsx
+++ src/components/Generic/Input/InputField.tsx
@@ -1,4 +1,5 @@
 import React, { FC, ReactNode } from "react";
+import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 interface InputProps {
   type?: "text" | "number" | "email" | "password" | "date" | "time" | string;
@@ -12,6 +13,7 @@ interface InputProps {
   max?: string;
   step?: number;
   error?: boolean;
+  errorMessage?: string;
   startIcon?: ReactNode;
   endIcon?: ReactNode;
 }
@@ -28,29 +30,35 @@ const Input: FC<InputProps> = ({
   max,
   step,
   error = false,
+  errorMessage = "Please fill in this field",
   startIcon,
   endIcon,
 }) => {
   
-  let inputClasses = `${error? 'ring-red-300 ring-2' : 'ring-muted-blue ring-1' } ${startIcon? 'pl-11': ''} w-full placeholder-muted-blue text-dark-blue-2 focus:outline-hidden focus:ring-3 rounded-lg px-4 py-2.5 text-sm shadow bg-white ${className}`;
+  let inputClasses = `${error? 'ring-pink-2 ring-2' : 'ring-muted-blue ring-1' } ${startIcon? 'pl-11': ''} w-full placeholder-muted-blue text-dark-blue-2 focus:outline-hidden focus:ring-3 rounded-lg px-4 py-2.5 text-sm shadow bg-white ${className}`;
 
   return (
-    <div className="relative">
-      {startIcon && <span className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5">{startIcon}</span>}
-      <input
-        type={type}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        defaultValue={defaultValue}
-        onChange={onChange}
-        min={min}
-        max={max}
-        step={step}
-        className={inputClasses}
-      />
-      {endIcon && <span className="absolute right-4 top-1/2 -translate-y-1/2 h-5 w-5">{endIcon}</span>}
-    </div>
+    <div>
+        <div className="relative">
+        {startIcon && <span className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5">{startIcon}</span>}
+        <input
+            type={type}
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            defaultValue={defaultValue}
+            onChange={onChange}
+            min={min}
+            max={max}
+            step={step}
+            className={inputClasses}
+        />
+        {endIcon && <span className="absolute right-4 top-1/2 -translate-y-1/2 h-5 w-5">{endIcon}</span>}
+        </div>
+        <div className="text-xs text-pink-2 flex gap-2 items-center h-5 py-4">
+            <ExclamationTriangleIcon className="w-3 h-3"/>
+            <p>{errorMessage}</p></div>
+        </div>
   );
 };
 
