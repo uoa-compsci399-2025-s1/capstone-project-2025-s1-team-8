1253c852aec13a71b8a70c7d327e9536f03421e8
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-16 14:15:36 +1200

    added project card component

---
+++ src/components/Generic/ProjectCard/ProjectCard.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import ProjectCard from './ProjectCard'
+import { ProjectCardType } from './ProjectCard'
+
+const meta: Meta<typeof ProjectCard> = {
+    component: ProjectCard,
+    title: 'Generic/ProjectCard',
+    tags: ['autodocs'],
+    }
+
+export default meta
+
+type Story = StoryObj<typeof ProjectCard>
+
+const mockProject: ProjectCardType = {
+    id: 'project-1',
+    projectInfo: {
+      projectId: 'p1',
+      projectName: 'Chronobiology',
+      projectDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      client: {
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+      },
+      additionalClients: [],
+      desiredOutput: 'Functional prototype and documentation',
+      teamNumber: 2,
+      semesters: ['Semester 2 2025'],
+      submissionDate: new Date('2025-06-30'),
+    },
+};
+    
+export const Default: Story = {
+    args: {
+        ...mockProject,
+    },
+}
---
+++ src/components/Generic/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,81 @@
+import { UniqueIdentifier } from '@dnd-kit/core'
+import React, {ReactNode} from 'react'
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+interface BasicClientDTOPlaceholder {
+    name: string;
+    email: string;
+}
+
+interface ProjectDTOPlaceholder {
+    projectId: string;
+    projectName: string;
+    projectDescription: string;
+    client: BasicClientDTOPlaceholder;
+    additionalClients?: BasicClientDTOPlaceholder[];
+    desiredOutput: string;
+    specialRequirements?: string;
+    teamNumber: number;
+    semesters: string[];
+    submissionDate: Date;
+    desiredTeamSkills?: string;
+    // resources: ReactNode[] - what do we want this type to be?
+}
+
+export interface ProjectCardType {
+    id: UniqueIdentifier;
+    projectInfo: ProjectDTOPlaceholder;
+}
+
+const ProjectCard = ({ id, projectInfo }: ProjectCardType) => {
+    const {
+      attributes,
+      listeners,
+      setNodeRef,
+      transform,
+      transition,
+      isDragging,
+    } = useSortable({
+      id: id,
+      data: {
+        type: 'item',
+      },
+    });
+  
+    //TODO: onclick of three dots (or whole card - will this interfere with drag and drop??), show expanded project modal (maybe another endpoint call to fetch all project info? Pass in projectID)
+  
+    const truncatedDescription = projectInfo.projectDescription.slice(0, 80) + "...";
+  
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition: transition ?? 'transform 200ms ease',
+      zIndex: isDragging ? 50 : 'auto',
+    };
+  
+    return (
+      <div
+        ref={setNodeRef}
+        {...attributes}
+        style={style}
+        className={`relative w-full bg-light-beige rounded-lg ring-1 ring-deeper-blue p-4 overflow-hidden group cursor-pointer transition-all duration-300 ease-in-out ${
+          isDragging ? 'opacity-50' : 'hover:shadow-md'
+        }`}
+      >
+        <button {...listeners} className="text-left">
+          <div className="absolute inset-0 bg-gradient-to-t from-bright-blue to-light-beige opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out z-0 rounded-lg" />
+          <div className="relative z-10">
+            <p className="text-dark-blue-1 text-lg font-bold">{projectInfo.projectName}</p>
+            <p className="text-dark-blue-1">{projectInfo.client.name}</p>
+            <p className="text-grey-1 py-2">{truncatedDescription}</p>
+          </div>
+        </button>
+  
+        <button className="absolute top-5 right-5 text-dark-blue-1 hover:text-black focus:outline-none">
+            <img src="/three-dots.svg" alt="Three dots icon" className="h-6 w-6" />
+        </button>
+      </div>
+    );
+  };
+  
+  export default ProjectCard;
