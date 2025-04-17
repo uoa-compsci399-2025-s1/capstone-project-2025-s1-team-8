42dbaa9f5d1593b022b34f6f51e088dadf2fe2fd
joohuil <99111909+joohuil@users.noreply.github.com>
2025-04-18 01:28:13 +1200

    added client card component

---
+++ src/components/Generic/ClientCard/ClientCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface BasicProjectDTOPlaceholder {
+  name: string
+  client: string
+  description: string
+}
+
+export interface ClientDTOPlaceholder {
+  name: string
+  email: string
+  affiliation?: string
+  introduction?: string
+  projects?: BasicProjectDTOPlaceholder[]
+}
+
+const ClientCard: React.FC<ClientDTOPlaceholder> = (clientInfo) => {
+  return (
+    <div className="bg-gradient-to-r from-dark-blue-6 to-deeper-blue hover:from-[#35474c] hover:to-[#6d939d] w-full border-beige border border-solid flex flex-row justify-between p-6">
+      <div className="flex flex-row gap-4">
+        <p className="text-light-beige font-semibold text-xl">{clientInfo.name}</p>
+        <p className="text-light-beige text-base self-end">{clientInfo.email}</p>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default ClientCard
