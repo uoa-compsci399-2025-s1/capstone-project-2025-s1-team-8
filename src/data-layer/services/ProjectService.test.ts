1b5dc3126cb9fd780a99be930831cff690c5a9c4
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-17 19:35:23 +1200

    fix: get all projects
    Now using expect.arrayContaining as we don't really care about the order returned by `getAllProjects`

--- src/data-layer/services/ProjectService.test.ts
+++ src/data-layer/services/ProjectService.test.ts
@@ -22,7 +22,9 @@ describe('Testing all the project service methods', () => {
       data: mockProject2,
     })
 
-    expect(await projectService.getAllProjects()).toStrictEqual([project1, project2])
+    expect(await projectService.getAllProjects()).toEqual(
+      expect.arrayContaining([project1, project2]),
+    )
   })
 
   test('Get project by ID', async () => {
