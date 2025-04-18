9f9b045d87aea1706fe47de1eaa82bd074f43c30
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-18 17:37:58 +1200

    chore: update secrets to include public URL and google secrets

--- environment.d.ts
+++ environment.d.ts
@@ -9,6 +9,18 @@ declare global {
        * Secret used to secure Payload
        */
       PAYLOAD_SECRET: string
+      /**
+       * The Google Auth Client ID, view the [docs](https://github.com/uoa-compsci399-2025-s1/capstone-project-2025-s1-team-8/wiki/Backend-Architecture#google-oauth-authentication) on how to generate one.
+       */
+      GOOGLE_CLIENT_ID: string
+      /**
+       * The Google Auth Client Secret, view the [docs](https://github.com/uoa-compsci399-2025-s1/capstone-project-2025-s1-team-8/wiki/Backend-Architecture#google-oauth-authentication) on how to generate one.
+       */
+      GOOGLE_CLIENT_SECRET: string
+      /**
+       * The public URL of the website
+       */
+      NEXT_PUBLIC_URL: string
     }
   }
 }
