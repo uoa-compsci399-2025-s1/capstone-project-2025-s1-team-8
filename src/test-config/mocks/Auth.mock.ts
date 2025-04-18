6686527afe18070f0deb059e70f946b8cf6371b7
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-18 20:11:17 +1200

    feat: create auth mocks

--- src/test-config/mocks/Auth.mock.ts
+++ src/test-config/mocks/Auth.mock.ts
@@ -1,3 +1,29 @@
+import { Authentication } from '@/payload-types'
+import { CreateAuthenticationData } from '@/types/Collections'
+
 export const STATE_MOCK = 'coolstate'
 export const SCOPE_MOCK = ['scope1', 'scope2']
 export const REDIRECT_URI_MOCK = `https://accounts.google.com/o/oauth2/v2/auth?state=${STATE_MOCK}&scope=${SCOPE_MOCK}`
+
+export const authMock: Authentication = {
+  id: '67ff38a56a35e1b6cf43a681',
+  user: '67ff38a56a35e1b6cf43a681',
+  type: 'OAuth',
+  provider: 'google',
+  providerAccountId: '1234567890',
+  refreshToken: 'refreshToken',
+  accessToken: 'accessToken',
+  expiresAt: Date.now(),
+  updatedAt: new Date().toISOString(),
+  createdAt: new Date().toISOString(),
+}
+
+export const authCreateMock: CreateAuthenticationData = {
+  user: '67ff38a56a35e1b6cf43a681',
+  type: 'OAuth',
+  provider: 'google',
+  providerAccountId: '1234567890',
+  refreshToken: 'refreshToken',
+  accessToken: 'accessToken',
+  expiresAt: Date.now(),
+}
