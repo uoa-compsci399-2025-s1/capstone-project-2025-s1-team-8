f15496149e2b7576a9edcd91424300eeed5cf197
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-18 20:40:35 +1200

    merge formQuestionServices into formServices

--- src/data-layer/services/FormService.test.ts
+++ src/data-layer/services/FormService.test.ts
@@ -2,6 +2,7 @@ import { clearCollection, testPayloadObject } from '@/test-config/utils'
 import { FormService } from './FormService'
 import { formResponseCreateMock } from '@/test-config/mocks/Form.mock'
 import { formMock } from '@/test-config/mocks/Form.mock'
+import { formQuestionCreateMock, formQuestionUpdateMock } from '@/test-config/mocks/Form.mock'
 
 describe('Form service tests', () => {
   const formService = new FormService()
@@ -107,3 +108,63 @@ describe('Form service tests', () => {
     })
   })
 })
+
+describe('FormQuestion service test', () => {
+  const formQuestionService = new FormService()
+
+  afterEach(async () => {
+    await clearCollection(testPayloadObject, 'formQuestion')
+  })
+
+  it('create a formQuestion', async () => {
+    const newFormQuestion = await formQuestionService.createFormQuestion(formQuestionCreateMock)
+    const res = await testPayloadObject.findByID({
+      collection: 'formQuestion',
+      id: newFormQuestion.id,
+    })
+    expect(newFormQuestion).toEqual(res)
+  })
+
+  it('find a formQuestion by ID', async () => {
+    const createdFormQuestion = await formQuestionService.createFormQuestion(formQuestionCreateMock)
+    const fetchedFormQuestion = await formQuestionService.getFormQuestion(createdFormQuestion.id)
+    expect(fetchedFormQuestion).toEqual(createdFormQuestion)
+  })
+
+  it('not found - find formQuestion with nonexistent id', async () => {
+    await expect(formQuestionService.getFormQuestion('nonexistent_id')).rejects.toThrow('Not Found')
+  })
+
+  it('update a formQuestion by ID', async () => {
+    const createdUser = await formQuestionService.createFormQuestion(formQuestionCreateMock)
+    const updatedFormQuestion = await formQuestionService.updateformQuestion(
+      createdUser.id,
+      formQuestionUpdateMock,
+    )
+    expect(updatedFormQuestion.question).toEqual(formQuestionUpdateMock.question)
+  })
+
+  it('not found - update a formQuestion with a nonexistant ID', async () => {
+    await expect(
+      formQuestionService.updateformQuestion('nonexistent_id', formQuestionUpdateMock),
+    ).rejects.toThrow('Not Found')
+  })
+
+  it('delete a formQuestion by ID', async () => {
+    const createdFormQuestion = await formQuestionService.createFormQuestion(formQuestionCreateMock)
+    const deletedFormQuestion = await formQuestionService.deleteFormQuestion(createdFormQuestion.id)
+    await expect(
+      testPayloadObject.findByID({
+        collection: 'formQuestion',
+        id: createdFormQuestion.id,
+      }),
+    ).rejects.toThrow('Not Found')
+  })
+
+  it('not found - delete a formQuestion with a nonexistent ID', async () => {
+    await expect(formQuestionService.deleteFormQuestion('non existent ID')).rejects.toThrow(
+      'Not Found',
+    )
+  })
+})
+
