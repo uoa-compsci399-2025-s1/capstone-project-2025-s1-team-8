de2851f4e2d113fcf711f0b339c54c8d7bc2efe7
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-18 12:48:13 +1200

    refactor: move all form related mocks to relevant file

--- src/test-config/mocks/Form.mock.ts
+++ src/test-config/mocks/Form.mock.ts
@@ -1,13 +1,29 @@
 import { CreateFormResponseData } from '@/types/Collections'
 import { QuestionResponse } from '@/types/Form'
-import { formQuestionMock } from './FormQuestion.mock'
+import { FormQuestion } from '@/payload-types'
 import { mockClient1 } from './User.mock'
 
+/*
+ * The FormQuestion mocks
+ */
+export const formQuestionMock: FormQuestion = {
+  id: '67ff38a56a35e1b6cf43a681',
+  question: 'What do you want out of this project?',
+  updatedAt: new Date().toISOString(),
+  createdAt: new Date().toISOString(),
+}
+
+/*
+ * QuestionResponse mocks
+ */
 export const questionResponseMock: QuestionResponse = {
   question: formQuestionMock,
   answer: 'Would be good to have a MVP done by blah blah...',
 }
 
+/*
+ * FormResponse mocks
+ */
 export const formResponseCreateMock: CreateFormResponseData = {
   name: 'AI girlfriend finder',
   description: 'AI girlfriend finder is a cool project to find your perfect match!',
