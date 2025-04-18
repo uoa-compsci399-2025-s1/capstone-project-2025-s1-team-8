c9c26a9fc24214bb98f26c03bcf2176b3e301492
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-18 19:15:04 +1200

    test: google auth redirection endpoint
    Also tests headers and redirections

---
+++ src/app/api/auth/google/route.test.ts
@@ -0,0 +1,65 @@
+import { REDIRECT_URI_MOCK, SCOPE_MOCK, STATE_MOCK } from '@/test-config/mocks/Auth.mock'
+import { GET } from './route'
+import { redirect } from 'next/navigation'
+import { oauth2Client } from '@/business-layer/security/google'
+import * as nextHeaders from 'next/headers'
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+
+vi.mock('@/business-layer/security/google', () => ({
+  oauth2Client: {
+    generateAuthUrl: vi.fn().mockReturnValue(REDIRECT_URI_MOCK),
+  },
+  googleAuthScopes: SCOPE_MOCK,
+}))
+
+vi.mock('next/headers', () => ({
+  cookies: () => ({
+    set: vi.fn(),
+  }),
+}))
+
+// Mock next redirection
+vi.mock('next/navigation', () => ({
+  redirect: vi.fn(),
+}))
+
+describe('Google Auth tests', async () => {
+  const mockSet = vi.fn()
+  beforeAll(() => {
+    // stubGlobal changes the values of global variables
+    vi.stubGlobal('crypto', {
+      randomUUID: () => STATE_MOCK,
+    })
+
+    const mockCookieStore: Partial<ReadonlyRequestCookies> = {
+      set: mockSet,
+    }
+
+    vi.spyOn(nextHeaders, 'cookies').mockImplementation(
+      async () => mockCookieStore as ReadonlyRequestCookies,
+    )
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('should redirect to Google OAuth URL with state and scopes', async () => {
+    await GET()
+    expect(redirect).toHaveBeenCalled()
+    expect(mockSet).toHaveBeenCalledWith('state', STATE_MOCK)
+    expect(oauth2Client.generateAuthUrl).toHaveBeenCalledWith({
+      scope: SCOPE_MOCK,
+      include_granted_scopes: true,
+      state: STATE_MOCK,
+    })
+    expect(redirect).toHaveBeenCalledWith(
+      oauth2Client.generateAuthUrl({
+        scope: SCOPE_MOCK,
+        include_granted_scopes: true,
+        state: STATE_MOCK,
+      }),
+    )
+    expect(redirect).toHaveBeenCalledWith(REDIRECT_URI_MOCK)
+  })
+})
---
+++ src/test-config/mocks/Auth.mock.ts
@@ -0,0 +1,3 @@
+export const STATE_MOCK = 'coolstate'
+export const SCOPE_MOCK = ['scope1', 'scope2']
+export const REDIRECT_URI_MOCK = `https://accounts.google.com/o/oauth2/v2/auth?state=${STATE_MOCK}&scope=${SCOPE_MOCK}`
