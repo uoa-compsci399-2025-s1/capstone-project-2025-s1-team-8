3864c3017c98554de4e96857badacc6b51069baa
joohuil <99111909+joohuil@users.noreply.github.com>
2025-04-19 18:40:10 +1200

    feat: copy icon function and success state

--- src/components/Generic/ClientCard/ClientCard.tsx
+++ src/components/Generic/ClientCard/ClientCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FiCopy } from 'react-icons/fi'
+import React, { useState } from 'react'
+import { FiCopy, FiCheck } from 'react-icons/fi'
 
 interface BasicProjectDTOPlaceholder {
   name: string
@@ -16,13 +16,27 @@ export interface ClientDTOPlaceholder {
 }
 
 const ClientCard: React.FC<ClientDTOPlaceholder> = (clientInfo) => {
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = (email: string) => {
+    navigator.clipboard.writeText(email)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1000)
+  }
+
   return (
     <div className="bg-gradient-to-r from-denim-blue to-deeper-blue hover:from-[#35474c] hover:to-[#6d939d] w-full flex flex-row justify-between p-6">
       <div className="flex flex-row gap-4">
         <p className="text-light-beige font-semibold text-xl">{clientInfo.name}</p>
         <p className="text-light-beige text-base self-end">{clientInfo.email}</p>
       </div>
-      <FiCopy className="self-center size-5 text-muted-blue hover:text-light-beige" />
+      <button onClick={() => handleCopy(clientInfo.email)}>
+        {copied ? (
+          <FiCheck className="self-center size-5 text-light-beige" />
+        ) : (
+          <FiCopy className="self-center size-5 text-muted-blue hover:text-light-beige cursor-pointer" />
+        )}
+      </button>
     </div>
   )
 }
