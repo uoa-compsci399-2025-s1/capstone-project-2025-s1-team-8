00613bc3004d45f39febef6c7ab6409d8f6dacb3
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-20 11:17:59 +1200

    fixed semesterProjectCard story

--- src/components/Generic/ProjectCard/SemesterProjectCard.stories.tsx
+++ src/components/Generic/ProjectCard/SemesterProjectCard.stories.tsx
@@ -15,8 +15,7 @@ type Story = StoryObj<typeof SemesterProjectCard>
 const mockProject: ProjectDTOPlaceholder = {
   projectId: 'p1',
   projectName: 'Chronobiology',
-  projectDescription:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  projectDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   client: {
     name: 'John Doe',
     email: 'johndoe@gmail.com',
@@ -30,6 +29,6 @@ const mockProject: ProjectDTOPlaceholder = {
 
 export const Default: Story = {
   args: {
-    ...mockProject,
+    projectInfo: mockProject,
   },
 }
