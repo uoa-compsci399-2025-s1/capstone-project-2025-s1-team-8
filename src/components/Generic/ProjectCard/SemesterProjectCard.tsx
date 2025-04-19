c6179a525556a41455a9c2431be98bef55271d70
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-04-20 10:31:50 +1200

    Added semester cards

--- src/components/Generic/ProjectCard/SemesterProjectCard.tsx
+++ src/components/Generic/ProjectCard/SemesterProjectCard.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { ProjectDTOPlaceholder } from './ProjectCard'
 
-const SemesterProjectCard: React.FC<ProjectDTOPlaceholder> = (projectInfo) => {
+interface SemesterProjectCardProps {
+  projectInfo: ProjectDTOPlaceholder
+}
+
+const SemesterProjectCard: React.FC<SemesterProjectCardProps> = ({ projectInfo }) => {
   //TODO: onclick of card, show expanded project modal
 
   const truncatedDescription = projectInfo.projectDescription.slice(0, 100) + '...'
 
   return (
-    <div
-      className={`relative w-full bg-light-beige rounded-lg ring-1 ring-deeper-blue p-4 overflow-hidden group cursor-pointer transition-all duration-300 ease-in-out hover:shadow-md`}
-    >
+    <div className={`w-full bg-light-beige rounded-lg ring-1 ring-deeper-blue p-4 cursor-pointer`}>
       <div className="text-left">
-        <div className="relative z-10">
-          <p className="text-dark-blue-1 text-lg font-bold">{projectInfo.projectName}</p>
-          <p className="text-dark-blue-1">{projectInfo.client.name}</p>
-          <p className="text-grey-1 py-2">{truncatedDescription}</p>
-        </div>
+        <p className="text-dark-blue-1 text-md font-bold">{projectInfo.projectName}</p>
+        <p className="text-dark-blue-1 text-sm">{projectInfo.client.name}</p>
+        <p className="text-grey-1 py-2 text-sm">{truncatedDescription}</p>
       </div>
     </div>
   )
