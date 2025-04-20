0c79b6bd62f2ca1737de0cc1789f5d240aefb91f
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-20 12:28:30 +1200

    fix: set http only and secure in cookie store

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -7,6 +7,8 @@ export const GET = async () => {
   const cookieStore = await cookies()
   // Set state to prevent CSRF attacks
   cookieStore.set('state', state, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
     /**
      * Should be set at less than 10 minutes but for security, best for 60 seconds or less
