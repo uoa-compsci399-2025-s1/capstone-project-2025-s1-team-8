07aa6e43b21811c020013687232f43b761902ccb
Sheena Lin <ensheanlin@gmail.com>
2025-04-21 11:57:13 +1200

    bug: getUser() is not working??

--- src/app/api/admin/users/[id]/route.test.ts
+++ src/app/api/admin/users/[id]/route.test.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 
 import { clearCollection, paramsToPromise, testPayloadObject } from '@/test-config/utils'
 import UserService from '@/data-layer/services/UserService'
-import { clientAdditionalInfoCreateMock } from '@/test-config/mocks/User.mock'
+import { clientAdditionalInfoCreateMock, clientCreateMock } from '@/test-config/mocks/User.mock'
 import { GET } from '@/app/api/admin/users/[id]/route'
 import { NextRequest } from 'next/server'
 
@@ -11,16 +11,25 @@ describe('admin fetch user', () => {
     await clearCollection(testPayloadObject, 'user')
   })
 
+  // currently the only ID that works
+  // 67fdc27019dd81b68db9bb64
   it('fetch user by Id', async () => {
     const userService = new UserService()
-    const user = await userService.createClientAdditionalInfo(clientAdditionalInfoCreateMock)
-    const id = typeof user.client === 'object' ? user.client.id : user.client
+    const newClient = await userService.createUser(clientCreateMock)
+    const newClientInfo = await userService.createClientAdditionalInfo({
+      ...clientAdditionalInfoCreateMock,
+      client: newClient,
+    })
+    const id = typeof newClientInfo.client === 'object' ? newClientInfo.client.id : newClientInfo.client
     const slug = { id }
     const res = await GET({} as NextRequest, {
       params: paramsToPromise(slug),
     })
+
+    const json = await res.json()
     expect(res.status).toBe(StatusCodes.OK)
-    expect(await res.json()).toEqual(user)
+    expect(await json.data.length).toEqual(1)
+    expect(await json).toEqual(newClientInfo)
   })
 
   it('should return a 404 error if the user does not exist', async () => {
--- src/app/api/admin/users/[id]/route.ts
+++ src/app/api/admin/users/[id]/route.ts
@@ -1,12 +1,14 @@
 import UserService from '@/data-layer/services/UserService'
 import { StatusCodes } from 'http-status-codes'
 import { NextRequest } from 'next/server'
+import { UserRole } from '@/types/User'
 
 /**
  * Fetches a single user by ID if the request is made by an admin
  *
  * @param param0 The ID of the user to fetch
  * @returns The user data
+ * @param _req
  */
 
 export const GET = async (
@@ -20,17 +22,16 @@ export const GET = async (
   const { id } = await params
   const userService = new UserService()
   try {
-    // const user = await userService.getUser(id)
-    const clientInfo = await userService.getClientAdditionalInfo(id)
-    // const clientInfo = {
-    //   client: user,
-    //   introduction: null,
-    //   affiliation: null,
-    // }
-    // const clientAdditionalInfo = await userService.createClientAdditionalInfo(clientInfo)
-    return Response.json(clientInfo)
+    const user = await userService.getUser(id)
+    console.log(user)
+    if (user.role === UserRole.Client) {
+      const { introduction, affiliation } = {...(await userService.getClientAdditionalInfo(id))}
+      return Response.json({ ...user, introduction, affiliation, })
+    }
+    return Response.json(user)
+
   } catch (error) {
-    if ((error as Error).message == 'Value is not JSON serializable') {
+    if ((error as Error).message == 'Value is not JSON serializable' || (error as Error).message == 'Not Found') {
       return Response.json({ error: 'User not found' }, { status: StatusCodes.NOT_FOUND })
     }
     return Response.json(
