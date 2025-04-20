14eca3b2af0640eeb01566fbaf062827738e94ed
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-20 21:13:05 +1200

    + prettier

--- src/app/api/project/[projectId]/route.ts
+++ src/app/api/project/[projectId]/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest } from "next/server"
-import { ProjectService } from "@/data-layer/services/ProjectService"
+import { NextRequest } from 'next/server'
+import { ProjectService } from '@/data-layer/services/ProjectService'
 import { StatusCodes } from 'http-status-codes'
 
 /**
@@ -9,19 +9,22 @@ import { StatusCodes } from 'http-status-codes'
  */
 
 export const GET = async (
-    req: NextRequest,
-    { params }: { params: {projectId: string} },
-  ): Promise<Response> => {
-    const { projectId } = await params
-    const projectService = new ProjectService()
+  req: NextRequest,
+  { params }: { params: { projectId: string } },
+): Promise<Response> => {
+  const { projectId } = await params
+  const projectService = new ProjectService()
 
-    try{
-        const data = await projectService.getProjectById(projectId)
-        return Response.json({data}, { status: StatusCodes.OK })
-    } catch (error) {
-        if ((error as Error).message == 'Not Found') {
-            return Response.json({ error: 'Project not found' }, { status: StatusCodes.NOT_FOUND })
-        }
-        return Response.json({ error: 'Internal Server Error' }, { status: StatusCodes.INTERNAL_SERVER_ERROR })
+  try {
+    const data = await projectService.getProjectById(projectId)
+    return Response.json({ data }, { status: StatusCodes.OK })
+  } catch (error) {
+    if ((error as Error).message == 'Not Found') {
+      return Response.json({ error: 'Project not found' }, { status: StatusCodes.NOT_FOUND })
     }
+    return Response.json(
+      { error: 'Internal Server Error' },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR },
+    )
+  }
 }
