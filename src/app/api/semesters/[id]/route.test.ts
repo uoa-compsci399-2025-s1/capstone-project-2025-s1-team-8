fa268c37c86f68fe3d90b083d747c6b984685f64
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-21 21:15:04 +1200

    test: admin PATCH semester/[id]

--- src/app/api/semesters/[id]/route.test.ts
+++ src/app/api/semesters/[id]/route.test.ts
@@ -1,33 +1,76 @@
 import { StatusCodes } from 'http-status-codes'
 
-import { clearCollection, paramsToPromise, testPayloadObject } from '@/test-config/utils'
+import {
+  clearCollection,
+  createMockNextPostRequest,
+  paramsToPromise,
+  testPayloadObject,
+} from '@/test-config/utils'
 import SemesterService from '@/data-layer/services/SemesterService'
 import { semesterCreateMock } from '@/test-config/mocks/Semester.mock'
-import { GET } from '@/app/api/semesters/[id]/route'
+import { GET, PATCH } from '@/app/api/semesters/[id]/route'
 import { NextRequest } from 'next/server'
 
-describe('get user count', () => {
+describe('/api/admin/semesters/[id]', () => {
+  const semesterService = new SemesterService()
+
   afterEach(async () => {
     await clearCollection(testPayloadObject, 'semester')
   })
 
-  it('should get a semester correctly', async () => {
-    const semesterService = new SemesterService()
-    const newSem = await semesterService.createSemester(semesterCreateMock)
-    const slug = { id: newSem.id }
-    const res = await GET({} as NextRequest, {
-      params: paramsToPromise(slug),
+  describe('GET /api/admin/semesters/[id]', () => {
+    it('should get a semester correctly', async () => {
+      const newSem = await semesterService.createSemester(semesterCreateMock)
+      const slug = { id: newSem.id }
+      const res = await GET({} as NextRequest, {
+        params: paramsToPromise(slug),
+      })
+      expect(res.status).toBe(StatusCodes.OK)
+      expect(await res.json()).toEqual({ data: newSem })
     })
-    expect(res.status).toBe(StatusCodes.OK)
-    expect(await res.json()).toEqual({ data: newSem })
+
+    it('should return a 404 error if the semester does not exist', async () => {
+      const slug = { id: 'nonexistent' }
+      const res = await GET({} as NextRequest, {
+        params: paramsToPromise(slug),
+      })
+      expect(res.status).toBe(StatusCodes.NOT_FOUND)
+      expect(await res.json()).toEqual({ error: 'Semester not found' })
+    })
   })
 
-  it('should return a 404 error if the semester does not exist', async () => {
-    const slug = { id: 'nonexistent' }
-    const res = await GET({} as NextRequest, {
-      params: paramsToPromise(slug),
+  describe('PATCH /api/admin/semesters/[id]', () => {
+    it('should update a semester correctly', async () => {
+      const newSem = await semesterService.createSemester(semesterCreateMock)
+      const updatedSem = { name: 'Updated Semester' }
+
+      const res = await PATCH(createMockNextPostRequest('', updatedSem), {
+        params: paramsToPromise({ id: newSem.id }),
+      })
+      const fetchedSem = await semesterService.getSemester(newSem.id)
+      expect(res.status).toBe(StatusCodes.OK)
+      expect(fetchedSem.name).toEqual(updatedSem.name)
+      expect(fetchedSem.updatedAt).not.toEqual(newSem.updatedAt)
+      expect(fetchedSem.createdAt).toEqual(newSem.createdAt)
     })
-    expect(res.status).toBe(StatusCodes.NOT_FOUND)
-    expect(await res.json()).toEqual({ error: 'Semester not found' })
+
+    it("should return a 400 error if the request body is invalid", async () => {
+      const newSem = await semesterService.createSemester(semesterCreateMock)
+      const updatedSem = { deadline: 1 }
+
+      const res = await PATCH(createMockNextPostRequest('', updatedSem), {
+        params: paramsToPromise({ id: newSem.id }),
+      })
+      expect(res.status).toBe(StatusCodes.BAD_REQUEST)
+      expect(await res.json()).toEqual({ error: 'Invalid request body' })
+    })
+
+    it("should return a 404 error if the semester does not exist", async () => {
+      const res = await PATCH(createMockNextPostRequest('', { name: 'Updated Semester' }), {
+        params: paramsToPromise({ id: 'nonexistent' }),
+      })
+      expect(res.status).toBe(StatusCodes.NOT_FOUND)
+      expect(await res.json()).toEqual({ error: 'Semester not found' })
+    })
   })
 })
