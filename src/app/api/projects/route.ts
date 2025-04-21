4230d2150d15c7901e4e0789690190d9d8d67a65
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-21 13:26:09 +1200

    Update route.ts

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,11 +1,20 @@
 import ProjectService from '@/data-layer/services/ProjectService'
+import { NextRequest } from 'next/server'
 
 /**
  * Fetches all projects
  * @param req - The request object.
  */
 
-export const GET = async (): Promise<Response> => {
+export const GET = async (req: NextRequest): Promise<Response> => {
   const projectService = new ProjectService()
+  const searchParams = req.nextUrl.searchParams
+  const clientId = searchParams.get('clientId')
+  const name = searchParams.get('name')
+  if (clientId){
+   return Response.json({ data: await projectService.getProjectsByClientId(clientId) }) 
+  } else if (name) {
+    return Response.json({ data: await projectService.getProjectByName(name) })
+  }
   return Response.json({ data: await projectService.getAllProjects() })
 }
