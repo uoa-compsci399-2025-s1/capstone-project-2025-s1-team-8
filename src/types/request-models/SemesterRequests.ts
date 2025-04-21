bc64f08052c72f1f559aa9eaa2caa6550b36cd52
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-21 22:22:28 +1200

    112 Create admin endpoint to update a semester (#143)
    * feat: add PATCH endpoint for updating a semeste
    
    - Implement PATCH handler to update semester details
    - Validate request body using Zod schema
    - Handle errors for invalid input, not found, and server issues
    - Add UpdateSemesterRequestBody schema for request validation
    
    * refactor: use logic from 4a139b7
    
    * test: admin PATCH semester/[id]
    
    * style: run prettier
    
    * ```
    refactor: Improve error handling for invalid request bodies and dates
    
    - Add detailed error messages for invalid date formats in ISO 8601
    - Include flattened error details in the response for Zod validation
    ```
    
    * refactor: Improve error handling for invalid request bodies and dates
    
    - Add detailed error messages for invalid date formats in ISO 8601
    - Include flattened error details in the response for Zod validation
    
    * test: improve error response assertions
    
    Update tests to destructure JSON responses and add detailed checks
    for error messages and field validation errors.
    
    * fix: missing closing brackets after merge

--- src/types/request-models/SemesterRequests.ts
+++ src/types/request-models/SemesterRequests.ts
@@ -1,5 +1,28 @@
 import { z } from 'zod'
 
+export const UpdateSemesterRequestBody = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  deadline: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), {
+      message: 'Invalid date format, should be in ISO 8601 format',
+    })
+    .optional(),
+  startDate: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), {
+      message: 'Invalid date format, should be in ISO 8601 format',
+    })
+    .optional(),
+  endDate: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), {
+      message: 'Invalid date format, should be in ISO 8601 format',
+    })
+    .optional(),
+})
+
 export const CreateSemesterRequestBody = z.object({
   name: z.string(),
   description: z.string().optional(),
