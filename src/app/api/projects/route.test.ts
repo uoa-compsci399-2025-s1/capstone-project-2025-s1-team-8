82733bb1f5590112edefadf19a0c8f8128701e3c
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-21 21:44:51 +1200

    Endpoint to create project (#132)
    * Add tests and route
    
    * prettier
    
    * Fix nit
    
    * Update route.test.ts
    
    * fix nit
    
    * prettier

---
+++ src/app/api/projects/route.test.ts
@@ -0,0 +1,36 @@
+import { StatusCodes } from 'http-status-codes'
+import {
+  clearCollection,
+  createMockNextRequestWithBody,
+  testPayloadObject,
+} from '@/test-config/utils'
+import ProjectService from '@/data-layer/services/ProjectService'
+import { projectCreateMock } from '@/test-config/mocks/Project.mock'
+import { POST } from '@/app/api/projects/route'
+
+describe('test POST /api/projects', () => {
+  afterEach(async () => {
+    await clearCollection(testPayloadObject, 'project')
+  })
+
+  it('should create a project', async () => {
+    const projectService = new ProjectService()
+    const req = createMockNextRequestWithBody(
+      'https://localhost:3000/api/projects',
+      projectCreateMock,
+    )
+    const res = await POST(req)
+    expect(res.status).toBe(StatusCodes.CREATED)
+    const project = (await res.json()).data
+    expect(project).toEqual(await projectService.getProjectById(project.id))
+  })
+
+  it('should fail to create a project', async () => {
+    const req = createMockNextRequestWithBody('https://localhost:3000/api/projects', {
+      ...projectCreateMock,
+      description: undefined,
+    })
+    const res = await POST(req)
+    expect(res.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+  })
+})
