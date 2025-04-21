c430789ba56af1e2276ab9e947d6167672e009f3
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-21 22:05:43 +1200

    style: run prettier

--- src/app/api/projects/route.test.ts
+++ src/app/api/projects/route.test.ts
@@ -1,9 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
-import {
-  clearCollection,
-  createMockNextPostRequest,
-  testPayloadObject,
-} from '@/test-config/utils'
+import { clearCollection, createMockNextPostRequest, testPayloadObject } from '@/test-config/utils'
 import ProjectService from '@/data-layer/services/ProjectService'
 import { projectCreateMock } from '@/test-config/mocks/Project.mock'
 import { POST } from '@/app/api/projects/route'
@@ -15,10 +11,7 @@ describe('test POST /api/projects', () => {
 
   it('should create a project', async () => {
     const projectService = new ProjectService()
-    const req = createMockNextPostRequest(
-      'https://localhost:3000/api/projects',
-      projectCreateMock,
-    )
+    const req = createMockNextPostRequest('https://localhost:3000/api/projects', projectCreateMock)
     const res = await POST(req)
     expect(res.status).toBe(StatusCodes.CREATED)
     const project = (await res.json()).data
