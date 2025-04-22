ac7d6f3977166e6b4ca0693b82366f096d69be82
Sheena Lin <ensheanlin@gmail.com>
2025-04-22 20:22:41 +1200

    implement NextResponse instead of Response

--- src/app/api/admin/users/[id]/route.ts
+++ src/app/api/admin/users/[id]/route.ts
@@ -1,9 +1,14 @@
 import UserService from '@/data-layer/services/UserService'
 import { StatusCodes } from 'http-status-codes'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { UserRole } from '@/types/User'
 import { NotFound } from 'payload'
 import { UpdateUserRequestBody } from '@/types/request-models/UserRequests'
+
+// also return UserCombinedInfo object instead of ClientInfo
+// admins can edit themselves lmaoooo
+
+// remove slug and remove bodyJson varaiable
 
 /**
  * Fetches a single user by ID if the request is made by an admin
@@ -27,18 +32,18 @@ export const GET = async (
     const user = await userService.getUser(id)
     if (user.role === UserRole.Client) {
       const { introduction, affiliation } = { ...(await userService.getClientAdditionalInfo(id)) }
-      return Response.json({ ...user, introduction, affiliation })
+      return NextResponse.json({ ...user, introduction, affiliation })
     }
-    return Response.json(user)
+    return NextResponse.json(user)
   } catch (error) {
     if (
       (error as Error).message === 'Value is not JSON serializable' ||
       (error as Error).message === 'Not Found' ||
       error === NotFound
     ) {
-      return Response.json({ error: 'User not found' }, { status: StatusCodes.NOT_FOUND })
+      return NextResponse.json({ error: 'User not found' }, { status: StatusCodes.NOT_FOUND })
     }
-    return Response.json(
+    return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: StatusCodes.INTERNAL_SERVER_ERROR },
     )
@@ -67,35 +72,36 @@ export const PATCH = async (
     const user = await userService.getUser(id)
     const body = UpdateUserRequestBody.parse(await _req.json())
     const updatedUser = await userService.updateUser(id, body)
+
     if (user.role === UserRole.Client) {
       const clientInfo = await userService.getClientAdditionalInfo(id)
-      /** @Todo write test for when clientInfo doesn't exist **/
-      if (clientInfo === null || clientInfo === undefined) {
+      if (!clientInfo) {
         const newClientInfo = await userService.createClientAdditionalInfo({
           client: id,
           introduction: null,
           affiliation: null,
         })
-        return Response.json(newClientInfo)
+        return NextResponse.json(newClientInfo)
       } else {
         const updatedClientInfo = await userService.updateClientAdditionalInfo(clientInfo.id, {
           introduction: body.introduction,
           affiliation: body.affiliation,
         })
-        return Response.json(updatedClientInfo)
+        return NextResponse.json(updatedClientInfo)
       }
-    } else if (user.role === UserRole.Student) {
-      return Response.json(updatedUser)
     }
-    return Response.json({ error: 'Unauthorized' }, { status: StatusCodes.UNAUTHORIZED })
+    else if (user.role === UserRole.Student) {
+      return NextResponse.json(updatedUser)
+    }
+
   } catch (error) {
     if (
       (error as Error).message == 'Value is not JSON serializable' ||
       (error as Error).message == 'Not Found'
     ) {
-      return Response.json({ error: 'User not found' }, { status: StatusCodes.NOT_FOUND })
+      return NextResponse.json({ error: 'User not found' }, { status: StatusCodes.NOT_FOUND })
     }
-    return Response.json(
+    return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: StatusCodes.INTERNAL_SERVER_ERROR },
     )
