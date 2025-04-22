156554b6c67ee4952998bcf698851b84393c8e3f
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-22 20:07:14 +1200

    111 Create endpoint for admins to delete semester (#147)
    * feat: create delete semester route and relevant tests
    
    * docs: delete method jsdoc

---
+++ src/app/api/admin/semesters/[id]/route.ts
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes'
+import { NextRequest, NextResponse } from 'next/server'
+import { NotFound } from 'payload'
+
+import SemesterService from '@/data-layer/services/SemesterService'
+
+/**
+ * DELETE method to delete a semester.
+ *
+ * @param req The request object containing the request body
+ * @returns No content status code
+ */
+export const DELETE = async (
+  _req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  try {
+    const { id } = await params
+    const semesterService = new SemesterService()
+    await semesterService.deleteSemester(id)
+    return new NextResponse(null, { status: StatusCodes.NO_CONTENT })
+  } catch (error) {
+    if (error instanceof NotFound) {
+      return NextResponse.json({ error: 'Semester not found' }, { status: StatusCodes.NOT_FOUND })
+    } else {
+      console.error(error)
+      return NextResponse.json(
+        { error: 'Internal server error' },
+        { status: StatusCodes.INTERNAL_SERVER_ERROR },
+      )
+    }
+  }
+}
