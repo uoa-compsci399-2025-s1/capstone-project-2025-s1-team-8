ac8f5a79d01f0c0f3bfcb2c2f7251b875c31117a
Sheena Lin <ensheanlin@gmail.com>
2025-04-23 21:06:03 +1200

    refactor

--- src/app/api/semesters/route.test.ts
+++ src/app/api/semesters/route.test.ts
@@ -27,26 +27,26 @@ describe('tests /api/semesters', () => {
       const data = await res.json()
       expect(data.data.length).toEqual(2)
     })
-  })
 
-  it('should return a list of all semesters created with pagination', async () => {
-    await semesterService.createSemester(semesterCreateMock)
-    await semesterService.createSemester(semesterCreateMock)
-    await semesterService.createSemester(semesterCreateMock)
-    const res1 = await GET(
-      createMockNextRequest('http://localhost:3000/api/semester?page=2&limit=2'),
-    )
-    expect(res1.status).toBe(StatusCodes.OK)
-    const data1 = await res1.json()
-    expect(data1.data.length).toEqual(1)
-    expect(data1.nextPage).toBeNull()
+    it('should return a list of all semesters created with pagination', async () => {
+      await semesterService.createSemester(semesterCreateMock)
+      await semesterService.createSemester(semesterCreateMock)
+      await semesterService.createSemester(semesterCreateMock)
+      const res1 = await GET(
+        createMockNextRequest('http://localhost:3000/api/semester?page=2&limit=2'),
+      )
+      expect(res1.status).toBe(StatusCodes.OK)
+      const data1 = await res1.json()
+      expect(data1.data.length).toEqual(1)
+      expect(data1.nextPage).toBeNull()
 
-    const res = await GET(
-      createMockNextRequest('http://localhost:3000/api/semester?page=1&limit=1'),
-    )
-    expect(res.status).toBe(StatusCodes.OK)
-    const data = await res.json()
-    expect(data.data.length).toEqual(1)
-    expect(data.nextPage).not.toBeNull()
+      const res = await GET(
+        createMockNextRequest('http://localhost:3000/api/semester?page=1&limit=1'),
+      )
+      expect(res.status).toBe(StatusCodes.OK)
+      const data = await res.json()
+      expect(data.data.length).toEqual(1)
+      expect(data.nextPage).not.toBeNull()
+    })
   })
 })
