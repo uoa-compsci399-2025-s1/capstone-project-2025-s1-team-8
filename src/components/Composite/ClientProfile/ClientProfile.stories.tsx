5b071d0b5d35f8fa0fe7f26b5c08b2fa017d4ce4
joohuil <99111909+joohuil@users.noreply.github.com>
2025-04-24 00:05:16 +1200

    refactor: use mock client

--- src/components/Composite/ClientProfile/ClientProfile.stories.tsx
+++ src/components/Composite/ClientProfile/ClientProfile.stories.tsx
@@ -1,22 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import ClientProfile from './ClientProfile'
-import { ClientDTOPlaceholder } from '@/components/Generic/ClientCard/ClientCard'
-
-const mockClient: ClientDTOPlaceholder =
-    {
-      name: 'David Cumin',
-      email: 'davidcumin@gmail.com',
-      affiliation: 'University of Auckland',
-      introduction:
-        'Hello! My name is John Doe and I am a lecturer at the University of Auckland. \n\nMy hobbies include snorkelling, fishing, reading, baking, eating, sleeping and taking various methods of transport to my destination! FIRE \n\nJohn Doe is a common placeholder name used to represent an unknown or anonymous individual, particularly in legal or informal contexts. It\'s often used as a stand-in when a person\'s real name is not known, needs to be protected, or when a general example is needed. The female equivalent is "Jane Doe". ',
-      projects: [
-        {
-          name: 'Chronobiology',
-          client: 'David Cumin',
-          description: 'A web based laboratory for visualisation and analysis of data.',
-        },
-      ],
-    }
+import { mockClients } from '@/mocks/Clients.mock'
 
 const meta: Meta<typeof ClientProfile> = {
   component: ClientProfile,
@@ -30,6 +14,6 @@ type Story = StoryObj<typeof ClientProfile>
 
 export const Default: Story = {
   args: {
-    clientInfo: mockClient,
+    clientInfo: mockClients[0],
   },
 }
