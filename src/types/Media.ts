de0f76e5f94345da3cf24ba7f5911472aab54227
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-23 19:19:06 +1200

    feat: create media and user zod type

---
+++ src/types/Media.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+// Payload Media type
+export const MediaSchema = z.object({
+  id: z.string(),
+  alt: z.string(),
+  updatedAt: z.string(),
+  createdAt: z.string(),
+  url: z.string().nullable().optional(),
+  thumbnailURL: z.string().nullable().optional(),
+  filename: z.string().nullable().optional(),
+  mimeType: z.string().nullable().optional(),
+  filesize: z.number().nullable().optional(),
+  width: z.number().nullable().optional(),
+  height: z.number().nullable().optional(),
+  focalX: z.number().nullable().optional(),
+  focalY: z.number().nullable().optional(),
+})
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,5 +1,26 @@
+import { z } from 'zod';
+import { MediaSchema } from './request-models/ProjectRequests';
+
 export enum UserRole {
   Admin = 'admin',
   Client = 'client',
   Student = 'student',
 }
+
+// Payload User type
+export const UserSchema = z.object({
+  id: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  role: z.enum(Object.values(UserRole) as [string, ...string[]]),
+  image: z.union([z.string(), MediaSchema]).nullable().optional(),
+  updatedAt: z.string(),
+  createdAt: z.string(),
+  email: z.string().email(),
+  resetPasswordToken: z.string().optional(),
+  resetPasswordExpiration: z.string().optional(),
+  salt: z.string().optional(),
+  hash: z.string().optional(),
+  lockUntil: z.string().optional(),
+  password: z.string().optional(),
+})
