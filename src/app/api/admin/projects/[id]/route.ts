f3aef9ce333df60da48f5b455a055ac9b327c323
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-24 20:24:06 +1200

    135-backend-create-endpoint-to-get-semester-projects (#152)
    * Make all project endpoints return NextResponse + add endpoint +  tests
    
    * Update ProjectResponses.ts
    
    * Update ProjectResponses.ts
    
    * Add filter by status and published
    
    * Update route.test.ts
    
    * Update ProjectService.test.ts
    
    * Merge all service methods into 1 + update endpoint
    
    * Fix minor logic error
    
    * Update ProjectService.ts
    
    * Update ProjectService.test.ts
    
    * Update ProjectService.test.ts
    
    * Update route.ts
    
    * remove response typing
    
    * P{rettier
    
    * Update route.ts

--- src/app/api/admin/projects/[id]/route.ts
+++ src/app/api/admin/projects/[id]/route.ts
@@ -2,19 +2,42 @@ import { StatusCodes } from 'http-status-codes'
 import { NextRequest, NextResponse } from 'next/server'
 import { NotFound } from 'payload'
 import { ZodError } from 'zod'
-
 import ProjectService from '@/data-layer/services/ProjectService'
 import { UpdateProjectRequestBody } from '@/types/request-models/ProjectRequests'
+
+/**
+ * Fetches a project by its ID.
+ * @param req - The request object.
+ * @param params - The parameters object containing the project ID.
+ */
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ projectId: string }> },
+) => {
+  const { projectId } = await params
+  const projectService = new ProjectService()
+
+  try {
+    const data = await projectService.getProjectById(projectId)
+    return NextResponse.json({ data })
+  } catch (error) {
+    if (error instanceof NotFound) {
+      return NextResponse.json({ error: 'Project not found' }, { status: StatusCodes.NOT_FOUND })
+    }
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR },
+    )
+  }
+}
 
 /**
  * Patches a project by its ID.
  * @param req - The request object.
  * @param params - The parameters object containing the project ID.
  */
-export const PATCH = async (
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string }> },
-): Promise<NextResponse> => {
+export const PATCH = async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params
   const projectService = new ProjectService()
   try {
@@ -43,10 +66,7 @@ export const PATCH = async (
  * @param req - The request object.
  * @param params - The parameters object containing the project ID.
  */
-export const DELETE = async (
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string }> },
-): Promise<NextResponse> => {
+export const DELETE = async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params
   const projectService = new ProjectService()
 
