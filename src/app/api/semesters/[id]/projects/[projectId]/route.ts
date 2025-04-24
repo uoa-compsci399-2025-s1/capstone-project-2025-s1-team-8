93eef7d7a69e42714f2fce1572f5f5f4caac67b4
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-24 22:16:52 +1200

    Add route

---
+++ src/app/api/semesters/[id]/projects/[projectId]/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { StatusCodes } from 'http-status-codes'
+
+import ProjectService from '@/data-layer/services/ProjectService'
+import { NotFound } from 'payload'
+import { SemesterSchema } from '@/types/Payload'
+
+/**
+ * Gets a semester project by its ID.
+ * @param req - The request object.
+ * @param params - The parameters object containing the semester ID and project ID.
+ * @returns A JSON response containing the list of projects and the next page cursor.
+ */
+export const GET = async (req: NextRequest, { params }: { params: Promise<{ id: string, projectId: string }> }) => {
+  const {id, projectId} = await params
+  console.log('id', id)
+    console.log('projectId', projectId)
+  const projectService = new ProjectService()
+  try{
+    const project = await projectService.getSemesterProject(projectId)
+    if (project.semester instanceof String && project.semester !== id) {
+        return NextResponse.json({ error: 'Project not found in this semester' }, { status: StatusCodes.NOT_FOUND })
+    } 
+    
+    const semester = SemesterSchema.safeParse(project.semester)
+    if (semester.success) {
+        if (semester.data.id !== id) {
+            return NextResponse.json({ error: 'Project not found in this semester' }, { status: StatusCodes.NOT_FOUND })
+        }
+    }
+    return NextResponse.json({ data: project })
+    
+  } catch (error) {
+    if (error instanceof NotFound) {
+      return NextResponse.json({ error: 'Project not found' }, { status: StatusCodes.NOT_FOUND })
+    } 
+    console.error('Error fetching project:', error)
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR },
+    )
+  }
+}
