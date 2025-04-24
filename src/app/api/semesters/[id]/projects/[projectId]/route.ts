5ac4dea519e3c285100a2a8c096f84f44e9f2255
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-24 22:19:34 +1200

    Add route

---
+++ src/app/api/semesters/[id]/projects/[projectId]/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { StatusCodes } from 'http-status-codes'
+
+import ProjectService from '@/data-layer/services/ProjectService'
+import { NotFound } from 'payload'
+import { ZodError } from 'zod'
+import { PatchSemesterProjectRequestBody } from '@/types/request-models/ProjectRequests'
+
+/**
+ * Updates a semester project by its ID.
+ * @param req - The request object.
+ * @param params - The parameters object containing the semester ID.
+ * @returns A JSON response containing the list of projects and the next page cursor.
+ */
+export const PATCH = async (req: NextRequest, { params }: { params: Promise<{ id: string, projectId: string }> }) => {
+  const paramData = await params
+  const projectService = new ProjectService()
+  try{
+    const data = PatchSemesterProjectRequestBody.parse(req.json())
+    const updatedProject = await projectService.updateSemesterProject(paramData.projectId, data)
+    return NextResponse.json({ data: updatedProject })
+  } catch (error) {
+    if (error instanceof NotFound) {
+      return NextResponse.json({ error: 'Project not found' }, { status: StatusCodes.NOT_FOUND })
+    } else if (error instanceof ZodError) {
+        return NextResponse.json(
+            { error: 'Invalid request body' },
+            { status: StatusCodes.BAD_REQUEST },
+        )
+        }
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR },
+    )
+  }
+}
