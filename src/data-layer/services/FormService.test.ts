8acc14095c7674b1bb805b9f69c34ca5dd21573f
Sheena Lin <ensheanlin@gmail.com>
2025-04-25 20:01:54 +1200

    removed tests that referred to formId in FormService.test

--- src/data-layer/services/FormService.test.ts
+++ src/data-layer/services/FormService.test.ts
@@ -17,22 +17,6 @@ describe('Form service tests', () => {
       expect(newForm).toEqual(fetchedForm)
     })
 
-    it('find a form by ID', async () => {
-      const createdForm = await formService.createForm(formMock)
-      const fetchedForm = await formService.getForm(createdForm.id)
-      expect(fetchedForm).toEqual(createdForm)
-    })
-
-    it('find the one form by ID', async () => {
-      const createdForm = await formService.createForm(formMock)
-      const fetchedForm = await formService.getAllForms()
-      expect(fetchedForm).toEqual(createdForm)
-    })
-
-    it('not found - find form with nonexistent id', async () => {
-      await expect(formService.getForm('nonexistent_id')).rejects.toThrow('Not Found')
-    })
-
     it('update a form by ID', async () => {
       const createdForm = await formService.createForm(formMock)
       const updatedForm = await formService.updateForm(createdForm.id, {
