66bd1a9f55c9a46fec42b88df48d804282820f0a
Sheena Lin <ensheanlin@gmail.com>
2025-04-25 15:29:37 +1200

    rebase

---
+++ src/app/api/admin/forms/route.ts
@@ -0,0 +1,46 @@
+import { ZodError } from 'zod'
+import { NotFound } from 'payload'
+import { StatusCodes } from 'http-status-codes'
+import { NextRequest, NextResponse } from 'next/server'
+
+import FormService from '@/data-layer/services/FormService'
+import { UpdateUserRequestBody } from '@/types/request-models/UserRequests'
+import { Security } from '@/business-layer/middleware/Security'
+import { UpdateFormRequestBody } from '@/types/request-models/FormRequest'
+import { UpdateFormData } from '@/types/Collections'
+
+class RouteWrapper {
+  /**
+   * Updates the form if the request is made by an admin
+   *
+   * @param _req
+   * @returns The updated form
+   */
+  @Security('jwt', ['admin'])
+  static async PATCH(_req: NextRequest) {
+    const formService = new FormService()
+    try {
+      const form = await formService.getAllForms()
+      const body = UpdateFormRequestBody.parse(await _req.json())
+      const updatedFormData: UpdateFormData = {...body}
+      const updatedForm = await formService.updateForm(form.id, updatedFormData)
+      return NextResponse.json(updatedForm)
+    } catch (error) {
+      if (error instanceof NotFound) {
+        return NextResponse.json({ error: 'Form not found' }, { status: StatusCodes.NOT_FOUND })
+      } else if (error instanceof ZodError) {
+        return NextResponse.json(
+          { error: 'Invalid request body', details: error.flatten() },
+          { status: StatusCodes.BAD_REQUEST },
+        )
+      }
+      console.error(error)
+      return NextResponse.json(
+        { error: 'Internal Server Error' },
+        { status: StatusCodes.INTERNAL_SERVER_ERROR },
+      )
+    }
+  }
+}
+
+export const PATCH = RouteWrapper.PATCH,
