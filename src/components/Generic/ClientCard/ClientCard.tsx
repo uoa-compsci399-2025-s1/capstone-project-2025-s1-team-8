76735b5a6f6e0618813417d536d0a8fe42d26868
joohuil <99111909+joohuil@users.noreply.github.com>
2025-04-25 15:36:57 +1200

    fix: clicking name opens modal

--- src/components/Generic/ClientCard/ClientCard.tsx
+++ src/components/Generic/ClientCard/ClientCard.tsx
@@ -27,12 +27,14 @@ const ClientCard: React.FC<ClientDTOPlaceholder> = (clientInfo) => {
 
   return (
     <>
-      <div
-        className="bg-gradient-to-r from-denim-blue to-deeper-blue hover:from-[#35474c] hover:to-[#6d939d] w-full flex flex-row justify-between p-6 cursor-pointer"
-        onClick={() => handleModal()}
-      >
+      <div className="bg-gradient-to-r from-denim-blue to-deeper-blue hover:from-[#35474c] hover:to-[#6d939d] w-full flex flex-row justify-between p-6">
         <div className="flex flex-row gap-4">
-          <p className="text-light-beige font-semibold text-xl">{clientInfo.name}</p>
+          <p 
+            className="text-light-beige font-semibold text-xl cursor-pointer"
+            onClick={() => handleModal()}
+          >
+            {clientInfo.name}
+          </p>
           <p className="text-light-beige text-base self-end">{clientInfo.email}</p>
         </div>
         <button onClick={() => handleCopy(clientInfo.email)}>
