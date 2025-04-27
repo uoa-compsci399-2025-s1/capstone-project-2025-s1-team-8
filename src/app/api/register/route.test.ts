db157c67cc5131fff9db9df90817df4a4c5f744b
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-27 17:32:39 +1200

    feat: Add user registration API with validation and conflict handling
    - POST /api/register endpoint
    - Zod schema for request body validation
    - Conflict response for existing users
    - Password hashing and user creation logic
    - Unit tests for successful registration and conflict scenarios

---
+++ src/app/api/register/route.test.ts
@@ -0,0 +1,55 @@
+import { StatusCodes } from 'http-status-codes'
+
+import { POST, RegisterRequestBody } from './route'
+import { createMockNextPostRequest } from '@/test-config/utils'
+import { UserRoleWithoutAdmin } from '@/types/User'
+import UserService from '@/data-layer/services/UserService'
+import AuthService from '@/data-layer/services/AuthService'
+
+describe('tests /api/register', () => {
+  const userService = new UserService()
+  const authService = new AuthService()
+
+  it('should register a new user', async () => {
+    const body: RegisterRequestBody = {
+      firstName: 'jeffery',
+      lastName: 'ji',
+      email: 'test@example.com',
+      password: 'password123',
+      role: UserRoleWithoutAdmin.Client,
+    }
+    const res = await POST(createMockNextPostRequest('/api/register', body))
+    const json = await res.json()
+
+    expect(res.status).toBe(StatusCodes.CREATED)
+    expect(json.message).toBe('User registered successfully')
+    expect(json.data).toHaveProperty('id')
+
+    const user = await userService.getUser(json.data.id)
+    expect(user.firstName).toEqual(body.firstName)
+    expect(user.lastName).toEqual(body.lastName)
+    expect(user.email).toEqual(body.email)
+    expect(user.role).toEqual(body.role)
+
+    const auth = await authService.getAuthByEmail(body.email)
+    expect(auth.password).not.toEqual(body.password)
+    expect(auth.type).toEqual("password")
+  })
+
+  it('should return a 409 conflict if user already exists', async () => {
+    const body: RegisterRequestBody = {
+      firstName: 'jeffery',
+      lastName: 'ji',
+      email: 'test@example.com',
+      password: 'password123',
+      role: UserRoleWithoutAdmin.Client,
+    }
+    await POST(createMockNextPostRequest('/api/register', body))
+
+    const res = await POST(createMockNextPostRequest('/api/register', body))
+    const json = await res.json()
+
+    expect(res.status).toBe(StatusCodes.CONFLICT)
+    expect(json.error).toBe('A user with that email already exists')
+  })
+})
---
+++ src/app/api/register/route.ts
@@ -0,0 +1,59 @@
+import AuthService from '@/business-layer/services/AuthService'
+import UserService from '@/data-layer/services/UserService'
+import AuthDataService from '@/data-layer/services/AuthService'
+import { CreateUserData } from '@/types/Collections'
+import { UserRoleWithoutAdmin } from '@/types/User'
+import { StatusCodes } from 'http-status-codes'
+import { NextRequest, NextResponse } from 'next/server'
+import { z, ZodError } from 'zod'
+
+export const RegisterRequestBodySchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  password: z.string(),
+  role: z.nativeEnum(UserRoleWithoutAdmin),
+})
+export type RegisterRequestBody = z.infer<typeof RegisterRequestBodySchema>
+
+export const POST = async (req: NextRequest) => {
+  const userService = new UserService()
+  const authService = new AuthService()
+  const authDataService = new AuthDataService()
+  try {
+    const body = RegisterRequestBodySchema.parse(await req.json())
+    const fetchedUser = await userService.getUserByEmail(body.email)
+    if (fetchedUser)
+      return NextResponse.json(
+        { error: 'A user with that email already exists' },
+        { status: StatusCodes.CONFLICT },
+      )
+    const user = await userService.createUser(body as CreateUserData)
+
+    const hash = await authService.hashPassword(body.password)
+    await authDataService.createAuth({
+      email: body.email,
+      password: hash,
+      type: 'password',
+    })
+
+    return NextResponse.json(
+      { message: 'User registered successfully', data: user },
+      {
+        status: StatusCodes.CREATED,
+      },
+    )
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        { error: 'Invalid request body', details: error.flatten() },
+        { status: StatusCodes.BAD_REQUEST },
+      )
+    }
+    console.error('Error', error)
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR },
+    )
+  }
+}
