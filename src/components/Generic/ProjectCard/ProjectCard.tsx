da1dd5099ca33940bfbfe2948eeb7440ce0738b0
joohuil <99111909+joohuil@users.noreply.github.com>
2025-04-27 17:22:52 +1200

    feat: modals to show when props line up

--- src/components/Generic/ProjectCard/ProjectCard.tsx
+++ src/components/Generic/ProjectCard/ProjectCard.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ProjectDTOPlaceholder } from './DraggableProjectCard'
+import ProjectModal from '@/components/Composite/ProjectModal/ProjectModal'
+
+// delete once bethany's branch is merged 
+
+export interface BasicClientDetails {
+  name: string
+  email: string
+}
+
+interface ProjectPlaceholder {
+  projectId: string
+  projectTitle: string
+  projectClientDetails: BasicClientDetails
+  otherClientDetails?: BasicClientDetails[]
+  projectDescription: string
+  desiredOutput: string
+  desiredTeamSkills: string
+  availableResources: string
+
+  specialRequirements: boolean
+  numberOfTeams: number
+  futureConsideration: boolean
+  Semesters: Array<string>
+  submittedDate: Date
+}
+
+// until here
 
 interface ProjectCardProps {
-  projectInfo: ProjectDTOPlaceholder
+  projectInfo: ProjectPlaceholder
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ projectInfo }) => {
-  //TODO: onclick of card, show expanded project modal
+  const [modalOpen, setModalOpen] = useState(false)
+
+  function toggleModal() {
+    setModalOpen(!modalOpen)
+  }
 
   const truncatedDescription =
     projectInfo.projectDescription.length > 100
@@ -14,14 +45,22 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ projectInfo }) => {
       : projectInfo.projectDescription
 
   return (
-    <div className="w-full bg-light-beige rounded-2xl ring-1 ring-deeper-blue p-5 pt-6 cursor-pointer">
-      <div className="text-left">
-        <div className="relative z-10">
-          <p className="text-dark-blue text-base font-semibold pb-0.5">{projectInfo.projectName}</p>
-          <p className="text-dark-blue text-xs">{projectInfo.client.name}</p>
-          <p className="text-grey-1 pt-3 pb-2 text-xs">{truncatedDescription}</p>
+    <div>
+      <div
+        className="w-full bg-light-beige rounded-2xl ring-1 ring-deeper-blue p-5 pt-6 cursor-pointer"
+        onClick={() => toggleModal()}
+      >
+        <div className="text-left">
+          <div className="relative z-10">
+            <p className="text-dark-blue text-base font-semibold pb-0.5">
+              {projectInfo.projectTitle}
+            </p>
+            <p className="text-dark-blue text-xs">{projectInfo.projectClientDetails.name}</p>
+            <p className="text-grey-1 pt-3 pb-2 text-xs">{truncatedDescription}</p>
+          </div>
         </div>
       </div>
+      <ProjectModal {...projectInfo} open={modalOpen} onClose={() => toggleModal()}/>
     </div>
   )
 }
