62b2741cf0ec6d5f36783fd167b28a0c919a5963
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-28 01:12:22 +1200

    prettier

--- src/app/(frontend)/auth/signin/page.tsx
+++ src/app/(frontend)/auth/signin/page.tsx
@@ -1,55 +1,56 @@
-"use client";
+'use client'
 
-import Button from "@/components/Generic/Button/Button"
-import Input from "@/components/Generic/Input/InputField"
-import { MdOutlineMail, MdLock } from "react-icons/md"
+import Button from '@/components/Generic/Button/Button'
+import Input from '@/components/Generic/Input/InputField'
+import { MdOutlineMail, MdLock } from 'react-icons/md'
 
 export default function SignIn() {
   return (
-
-    <div className = "flex flex-row justify-between items-center min-h-screen w-full">
+    <div className="flex flex-row justify-between items-center min-h-screen w-full">
       {/*Leftside*/}
       <div className="flex flex-col justify-center items-start pl-[10vw] min-h-screen w-6/10 bg-gradient-to-t from-[#88B8C4] via-[#A7CAD1] to-[#C6DCDE] gap-8">
         <p className="text-beige text-5xl font-bold font-silkscreen">ENCAPSULATE</p>
         <p className="text-beige text-xl">Submit your project proposal today!</p>
         <Button size="sm" variant="light">
-        Read more
+          Read more
         </Button>
       </div>
       {/*Rightside*/}
       <div className="flex flex-col justify-center items-center min-h-screen w-4/10">
-
-      <div className="flex flex-col justify-center items-start gap-4">
-      <p className="text-2xl font-bold text-dark-blue">WELCOME BACK</p>
-        <form>
-          <div className="flex flex-col gap-4">
-          <Input
-          onChange={() => {}}
-          placeholder="Email Address"
-          type="email"
-          startIcon={<MdOutlineMail className="text-muted-blue"/>}
-        />
-          <Input
-            onChange={() => {}}
-            placeholder="Password"
-            type="password"
-            startIcon={<MdLock className="text-muted-blue"/>}
-          />
-          <Button size="sm" variant="dark">
-            Sign In
-          </Button>
-          <p className="text-steel-blue">Don't have an account? Sign Up <a href="/auth/signup"><u>here</u></a></p>
-          <div className="flex items-center w-full">
-            <div className="border-t border-steel-blue flex-grow"></div>
-            <span className="mx-4 text-steel-blue">OR</span>
-            <div className="border-t border-steel-blue flex-grow"></div>
-          </div>
-          </div>
-        </form>
-
+        <div className="flex flex-col justify-center items-start gap-4">
+          <p className="text-2xl font-bold text-dark-blue">WELCOME BACK</p>
+          <form>
+            <div className="flex flex-col gap-4">
+              <Input
+                onChange={() => {}}
+                placeholder="Email Address"
+                type="email"
+                startIcon={<MdOutlineMail className="text-muted-blue" />}
+              />
+              <Input
+                onChange={() => {}}
+                placeholder="Password"
+                type="password"
+                startIcon={<MdLock className="text-muted-blue" />}
+              />
+              <Button size="sm" variant="dark">
+                Sign In
+              </Button>
+              <p className="text-steel-blue">
+                Don't have an account? Sign Up{' '}
+                <a href="/auth/signup">
+                  <u>here</u>
+                </a>
+              </p>
+              <div className="flex items-center w-full">
+                <div className="border-t border-steel-blue flex-grow"></div>
+                <span className="mx-4 text-steel-blue">OR</span>
+                <div className="border-t border-steel-blue flex-grow"></div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-      
-    	</div>
     </div>
   )
 }
