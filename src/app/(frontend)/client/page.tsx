db5abd55ccb69a72bd46d7838f983eb64b2c68f2
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-28 21:31:52 +1200

    Delete redundant mocks + add admin dashboard

--- src/app/(frontend)/client/page.tsx
+++ src/app/(frontend)/client/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 
 import NavBar from '@/components/Generic/NavBar/NavBar'
-import { mockClient } from './mockClient'
-import { mockProjects } from './mockProjects'
+import { mockClients } from '@/mocks/Clients.mock'
+import { mockProjects } from '@/mocks/Projects.mock'
 import ClientDashboard from '@/components/Pages/ClientDashboard/ClientDashboard'
 
 export default function Client() {
   return (
     <div className="flex justify-center items-center">
       <div>
-        <NavBar />
+        <NavBar  navElements={[{ href: '/admin', text: 'My Dashboard' }]} />
       </div>
       <div className="items-center justify-center w-[90vw] pt-40 pb-30">
-        <ClientDashboard client={mockClient} projects={mockProjects} />
+        <ClientDashboard client={mockClients[0]} projects={mockProjects} />
       </div>
     </div>
   )
