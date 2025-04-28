164200a7ab06d18838a72498d25877bd3ccb452c
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-28 15:06:05 +1200

    Reorganise logic to make nav bar right on top of sign in

--- src/app/(frontend)/auth/layout.tsx
+++ src/app/(frontend)/auth/layout.tsx
@@ -6,29 +6,31 @@ import Image from 'next/image'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-row justify-center lg:justify-between items-center min-h-screen w-full">
-      <nav className="py-2 px-[5%] pr-[8%] fixed top-0 left-0 w-full z-50">
-        <div className="container mx-auto flex items-center justify-start">
+    <div>
+      <div className="flex flex-row justify-center lg:justify-between items-center min-h-screen w-full">
+        {/*Leftside*/}
+        <div className="hidden lg:flex flex-col justify-center min-h-screen w-6/10 bg-gradient-to-t from-[#88B8C4] via-[#A7CAD1] to-[#C6DCDE]">
+          <div className="flex flex-col justify-center items-start pl-[10vw] gap-8">
+            <p className="text-beige text-5xl font-bold font-silkscreen">ENCAPSULATE</p>
+            <p className="text-beige text-xl">Submit your project proposal today!</p>
+            <Link href="/">
+              <Button size="sm" variant="light">
+                Read more
+              </Button>
+            </Link>
+          </div>
+        </div>
+        {/*Rightside*/}
+        <div className="flex flex-col justify-center items-center min-h-screen min-w-1 lg:w-4/10">
+        <nav className="py-2 px-[5%] pr-[8%] lg:fixed lg:top-0 lg:left-0 w-full z-50">
+        <div className="container mx-auto flex items-center justify-center lg:justify-start">
           <Link href="/" className="text-dark-blue">
             <Image src="/dark-logo.png" alt="logo" width={100} height={100} />
           </Link>
         </div>
       </nav>
-      {/*Leftside*/}
-      <div className="hidden lg:flex flex-col justify-center min-h-screen w-6/10 bg-gradient-to-t from-[#88B8C4] via-[#A7CAD1] to-[#C6DCDE]">
-        <div className="flex flex-col justify-center items-start pl-[10vw] gap-8">
-          <p className="text-beige text-5xl font-bold font-silkscreen">ENCAPSULATE</p>
-          <p className="text-beige text-xl">Submit your project proposal today!</p>
-          <Link href="/">
-            <Button size="sm" variant="light">
-              Read more
-            </Button>
-          </Link>
+          {children}
         </div>
-      </div>
-      {/*Rightside*/}
-      <div className="flex flex-col justify-center items-center min-h-screen min-w-1 lg:w-4/10">
-        {children}
       </div>
     </div>
   )
