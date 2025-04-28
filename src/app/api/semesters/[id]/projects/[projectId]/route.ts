567d0a704016fffcadde3bce29dcbc529d4ed211
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-29 00:20:34 +1200

    refactor: use route wrapper

--- src/app/api/semesters/[id]/projects/[projectId]/route.ts
+++ src/app/api/semesters/[id]/projects/[projectId]/route.ts
@@ -7,118 +7,124 @@ import { ZodError } from 'zod'
 import { PatchSemesterProjectRequestBody } from '@/types/request-models/ProjectRequests'
 import SemesterService from '@/data-layer/services/SemesterService'
 
-/**
- * Updates a semester project by its ID.
- * @param req - The request object.
- * @param params - The parameters object containing the semester ID.
- * @returns A JSON response containing the projects.
- */
-export const PATCH = async (
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string; projectId: string }> },
-) => {
-  const { id, projectId } = await params
-  const projectService = new ProjectService()
-  const semesterService = new SemesterService()
-  let project
-  let fetchedSemester
+class RouteWrapper {
+  /**
+   * Gets a semester project by its ID.
+   *
+   * @param req - The request object.
+   * @param params - The parameters object containing the semester ID and project ID.
+   * @returns A JSON response containing the project data
+   */
+  static async GET(
+    _req: NextRequest,
+    { params }: { params: Promise<{ id: string; projectId: string }> },
+  ) {
+    const { id, projectId } = await params
+    const projectService = new ProjectService()
+    const semesterService = new SemesterService()
+    let project
+    let fetchedSemester
 
-  try {
     try {
-      project = await projectService.getSemesterProject(projectId)
-    } catch (error) {
-      if (error instanceof NotFound) {
-        return NextResponse.json({ error: 'Project not found!' }, { status: StatusCodes.NOT_FOUND })
+      try {
+        project = await projectService.getSemesterProject(projectId)
+      } catch (error) {
+        if (error instanceof NotFound) {
+          return NextResponse.json({ error: 'Project not found!' }, { status: StatusCodes.NOT_FOUND })
+        }
+        throw error
       }
-      throw error
-    }
 
-    try {
-      fetchedSemester = await semesterService.getSemester(id)
-    } catch (error) {
-      if (error instanceof NotFound) {
+      try {
+        fetchedSemester = await semesterService.getSemester(id)
+      } catch (error) {
+        if (error instanceof NotFound) {
+          return NextResponse.json(
+            { error: 'Semester not found!' },
+            { status: StatusCodes.NOT_FOUND },
+          )
+        }
+        throw error
+      }
+      if (JSON.stringify(project.semester) !== JSON.stringify(fetchedSemester)) {
         return NextResponse.json(
-          { error: 'Semester not found!' },
-          { status: StatusCodes.NOT_FOUND },
+          { error: 'Project is not associated with semester!' },
+          { status: StatusCodes.BAD_REQUEST },
         )
       }
-      throw error
-    }
-
-    const data = PatchSemesterProjectRequestBody.parse(await req.json())
-    if (JSON.stringify(project.semester) !== JSON.stringify(fetchedSemester)) {
+      return NextResponse.json({ data: project })
+    } catch (error) {
+      console.error('Error:', error)
       return NextResponse.json(
-        { error: 'Project does not belong to this semester' },
-        { status: StatusCodes.BAD_REQUEST },
+        { error: 'Internal Server Error' },
+        { status: StatusCodes.INTERNAL_SERVER_ERROR },
       )
     }
+  }
 
-    const updatedProject = await projectService.updateSemesterProject(projectId, data)
-    return NextResponse.json({ data: updatedProject })
-  } catch (error) {
-    if (error instanceof ZodError) {
+  /**
+   * Updates a semester project by its ID.
+   *
+   * @param req - The request object.
+   * @param params - The parameters object containing the semester ID.
+   * @returns A JSON response containing the projects.
+   */
+  static async PATCH(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string; projectId: string }> },
+  ){
+    const { id, projectId } = await params
+    const projectService = new ProjectService()
+    const semesterService = new SemesterService()
+    let project
+    let fetchedSemester
+
+    try {
+      try {
+        project = await projectService.getSemesterProject(projectId)
+      } catch (error) {
+        if (error instanceof NotFound) {
+          return NextResponse.json({ error: 'Project not found!' }, { status: StatusCodes.NOT_FOUND })
+        }
+        throw error
+      }
+
+      try {
+        fetchedSemester = await semesterService.getSemester(id)
+      } catch (error) {
+        if (error instanceof NotFound) {
+          return NextResponse.json(
+            { error: 'Semester not found!' },
+            { status: StatusCodes.NOT_FOUND },
+          )
+        }
+        throw error
+      }
+
+      const data = PatchSemesterProjectRequestBody.parse(await req.json())
+      if (JSON.stringify(project.semester) !== JSON.stringify(fetchedSemester)) {
+        return NextResponse.json(
+          { error: 'Project does not belong to this semester' },
+          { status: StatusCodes.BAD_REQUEST },
+        )
+      }
+
+      const updatedProject = await projectService.updateSemesterProject(projectId, data)
+      return NextResponse.json({ data: updatedProject })
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return NextResponse.json(
+          { error: 'Invalid request body' },
+          { status: StatusCodes.BAD_REQUEST },
+        )
+      }
+      console.error('Error updating project:', error)
       return NextResponse.json(
-        { error: 'Invalid request body' },
-        { status: StatusCodes.BAD_REQUEST },
+        { error: 'Internal Server Error' },
+        { status: StatusCodes.INTERNAL_SERVER_ERROR },
       )
     }
-    console.error('Error updating project:', error)
-    return NextResponse.json(
-      { error: 'Internal Server Error' },
-      { status: StatusCodes.INTERNAL_SERVER_ERROR },
-    )
   }
 }
 
-/**
- * Gets a semester project by its ID.
- * @param req - The request object.
- * @param params - The parameters object containing the semester ID and project ID.
- * @returns A JSON response containing the project data
- */
-export const GET = async (
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string; projectId: string }> },
-) => {
-  const { id, projectId } = await params
-  const projectService = new ProjectService()
-  const semesterService = new SemesterService()
-  let project
-  let fetchedSemester
-
-  try {
-    try {
-      project = await projectService.getSemesterProject(projectId)
-    } catch (error) {
-      if (error instanceof NotFound) {
-        return NextResponse.json({ error: 'Project not found!' }, { status: StatusCodes.NOT_FOUND })
-      }
-      throw error
-    }
-
-    try {
-      fetchedSemester = await semesterService.getSemester(id)
-    } catch (error) {
-      if (error instanceof NotFound) {
-        return NextResponse.json(
-          { error: 'Semester not found!' },
-          { status: StatusCodes.NOT_FOUND },
-        )
-      }
-      throw error
-    }
-    if (JSON.stringify(project.semester) !== JSON.stringify(fetchedSemester)) {
-      return NextResponse.json(
-        { error: 'Project is not associated with semester!' },
-        { status: StatusCodes.BAD_REQUEST },
-      )
-    }
-    return NextResponse.json({ data: project })
-  } catch (error) {
-    console.error('Error:', error)
-    return NextResponse.json(
-      { error: 'Internal Server Error' },
-      { status: StatusCodes.INTERNAL_SERVER_ERROR },
-    )
-  }
-}
+export const GET = RouteWrapper.GET, PATCH = RouteWrapper.PATCH
