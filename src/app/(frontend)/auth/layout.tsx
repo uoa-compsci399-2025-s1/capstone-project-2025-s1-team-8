7950939ba786ac1e399303ddec24185c0f8ac4c4
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-28 12:41:11 +1200

    prettier

--- src/app/(frontend)/auth/layout.tsx
+++ src/app/(frontend)/auth/layout.tsx
@@ -9,11 +9,11 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     <div className="flex flex-row justify-between items-center min-h-screen w-full">
       <nav className="py-2 px-[5%] pr-[8%] fixed top-0 left-0 w-full z-50">
         <div className="container mx-auto flex items-center justify-start">
-        <Link href="/" className="text-dark-blue">
-          <Image src="/dark-logo.png" alt="logo" width={100} height={100} />
-        </Link>
+          <Link href="/" className="text-dark-blue">
+            <Image src="/dark-logo.png" alt="logo" width={100} height={100} />
+          </Link>
         </div>
-        </nav>
+      </nav>
       {/*Leftside*/}
       <div className="flex flex-col justify-center min-h-screen w-6/10 bg-gradient-to-t from-[#88B8C4] via-[#A7CAD1] to-[#C6DCDE]">
         <div className="flex flex-col justify-center items-start pl-[10vw] gap-8">
