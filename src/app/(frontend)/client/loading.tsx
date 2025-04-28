66253407bbe794572423a569e890dc266f85fa2f
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-28 20:06:11 +1200

    refactor: remove example code

--- src/app/(frontend)/client/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin"></div>
-    </div>
-  )
-}
--- src/app/(frontend)/client/page.tsx
+++ src/app/(frontend)/client/page.tsx
@@ -1,15 +1,3 @@
-import React, { Suspense } from 'react'
-import NewComponent from './new-component'
-import Loading from './loading'
-
 export default function Client() {
-  return (
-    <div>
-      This is the client page
-      <h1 style={{ fontSize: '50px' }}>SOME TITLE</h1>
-      <Suspense fallback={<Loading />}>
-        <NewComponent />
-      </Suspense>
-    </div>
-  )
+  return <div>This is the client page</div>
 }
