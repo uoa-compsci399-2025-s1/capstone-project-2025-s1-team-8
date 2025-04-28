3dca0e8b3e4acf5d5d76d0f537a2736ee8526182
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-28 19:29:03 +1200

    prettier

--- src/app/(frontend)/client/page.tsx
+++ src/app/(frontend)/client/page.tsx
@@ -1,21 +1,18 @@
 'use client'
 
-import NavBar from "@/components/Generic/NavBar/NavBar";
-import { mockClient } from "./mockClient";
-import { mockProjects } from "./mockProjects";
-import ClientDashboard from "@/components/Pages/ClientDashboard/ClientDashboard";
+import NavBar from '@/components/Generic/NavBar/NavBar'
+import { mockClient } from './mockClient'
+import { mockProjects } from './mockProjects'
+import ClientDashboard from '@/components/Pages/ClientDashboard/ClientDashboard'
 
 export default function Client() {
   return (
     <div className="flex justify-center items-center">
       <div>
-      <NavBar/>
+        <NavBar />
       </div>
       <div className="flex flex-col items-center justify-center gap-12 mx-6 my-6 w-[80vw] pt-24">
-      <ClientDashboard
-        client={mockClient}
-        projects={mockProjects}
-        />
+        <ClientDashboard client={mockClient} projects={mockProjects} />
       </div>
     </div>
   )
