1140d583a2942618693ade1b31e49a625c74c08e
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-28 19:51:42 +1200

    fix: export as const instead of class

--- src/lib/services/semester/SemesterService.ts
+++ src/lib/services/semester/SemesterService.ts
@@ -1,5 +1,3 @@
-'use server'
-
 import { Semester, SemesterProject } from '@/payload-types'
 import { GET as GetSemesters } from '@/app/api/semesters/route'
 import { GET as GetProjects } from '@/app/api/semesters/[id]/projects/route'
@@ -14,6 +12,7 @@ const SemesterService = {
   getAllPaginatedSemesters: async function (
     options: { page?: number; limit?: number } = {},
   ): Promise<{ data: Semester[]; nextPage?: string; error?: string }> {
+    'use server'
     const url = buildNextRequestURL('/api/semesters', options)
     const response = await GetSemesters(await buildNextRequest(url, { method: 'GET' }))
     const { data, nextPage, error } = { ...(await response.json()) }
@@ -23,6 +22,7 @@ const SemesterService = {
   getAllPaginatedProjectsBySemesterId: async function (
     semesterId: string,
   ): Promise<{ data: SemesterProject[]; nextPage?: string; error?: string }> {
+    'use server'
     const url = `/api/semesters/${semesterId}/projects`
     const response = await GetProjects(await buildNextRequest(url, { method: 'GET' }), {
       params: Promise.resolve({ id: semesterId }),
@@ -34,6 +34,7 @@ const SemesterService = {
   createSemester: async function (
     semester: CreateSemesterData,
   ): Promise<{ data: Semester; error?: string }> {
+    'use server'
     const url = '/api/admin/semesters'
     const response = await CreateSemester(
       await buildNextRequest(url, { method: 'POST', body: semester }),
@@ -46,6 +47,7 @@ const SemesterService = {
     semesterId: string,
     semester: UpdateSemesterData,
   ): Promise<{ data: Semester; error?: string }> {
+    'use server'
     const url = `/api/admin/semesters/${semesterId}`
     const response = await UpdateSemester(
       await buildNextRequest(url, { method: 'PATCH', body: semester }),
