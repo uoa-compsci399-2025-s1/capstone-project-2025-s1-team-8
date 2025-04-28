fc3a7b3371d8151b644a78fe1b971f3fec963ffc
Sheena Lin <ensheanlin@gmail.com>
2025-04-28 14:26:55 +1200

    changed padding

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 
 const Homepage: React.FC = () => {
   return (
-    <div className="h-dvh flex flex-col items-center space-y-8 pt-[100px]">
+    <div className="h-dvh flex flex-col items-center space-y-8">
       <NavBar />
-      <div className="flex flex-1 flex-col justify-center items-center space-y-8 px-[100px]">
+      <div className="flex flex-1 flex-col justify-center items-center space-y-8 p-10">
         <Image src="/homepage-text.svg" alt="ENCAPSULATE" width={713} height={288} priority />
         <Button variant="outline" size="sm">
           Submit your proposal
