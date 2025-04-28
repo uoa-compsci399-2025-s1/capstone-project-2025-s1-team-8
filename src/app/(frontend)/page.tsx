d04e3aa003be15aa027cf28dc42919c1b0db1de5
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-04-28 18:14:01 +1200

    chore: remove client redirection button

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -47,7 +47,6 @@ export default async function HomePage() {
           </a>
         </div>
       </div>
-      <a href="/client">Client</a>
       <div className="footer">
         <p>Update this page by editing</p>
         <a className="codeLink" href={fileURL}>
