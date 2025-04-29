243611600419dae51289122ca67153885d9fea20
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-30 11:20:39 +1200

    215 frontend create client dashboard page (#218)
    * prettier
    
    * Add mocks and initial page mockup
    
    * Replace page with premade client dashboard
    
    * prettier
    
    * add comments
    
    * Add link to forms page in component
    
    * Update dimensions a bit
    
    * prettier
    
    * Update padding a bit to reflect the figma a bit more
    
    * Add functionality to autofill project title in form
    
    * Update CreateProjectCard.tsx
    
    * Update CreateProjectCard.tsx
    
    * Delete redundant mocks + add admin dashboard
    
    * prettier
    
    * prettier
    
    * prettier
    
    * change links
    
    * Update input field component and page
    
    * Fix minro bug
    
    * fix: styling changes
    
    * fix: yassify
    
    * feat: nav bar shadow
    
    * fix: prettier
    
    * fix: hollow nav bar
    
    * Update page.tsx
    
    * Prettier + make it so that closing project form redirects to client page
    
    ---------
    
    Co-authored-by: joohuil <99111909+joohuil@users.noreply.github.com>

--- src/app/(frontend)/form/page.tsx
+++ src/app/(frontend)/form/page.tsx
@@ -8,6 +8,8 @@ import Checkbox from '@/components/Generic/Checkbox/Checkbox'
 import { semesterNames1 } from '@/mocks/Semesters.mock'
 import { FiCheck } from 'react-icons/fi'
 import { HiX } from 'react-icons/hi'
+import { useSearchParams } from 'next/navigation'
+import Link from 'next/link'
 
 interface OtherClientDetails {
   fullName: string
@@ -17,6 +19,8 @@ interface OtherClientDetails {
 export default function Form() {
   // State to manage the pairs of names and emails of additional clients
   const [pairs, setPairs] = useState<OtherClientDetails[]>([])
+  const searchParams = useSearchParams()
+  const projectName = searchParams.get('projectName') || ''
 
   const handleChange = (index: number, field: keyof OtherClientDetails, value: string) => {
     const updated = [...pairs]
@@ -36,12 +40,14 @@ export default function Form() {
     <div className="h-dvh w-dvw bg-gradient-to-b from-[#779ea7] to-[#dae6e2] flex flex-col items-center overflow-y-scroll py-[8%] px-[10%] gap-4 p-4">
       <div className="relative bg-light-beige max-w-full flex flex-col rounded-2xl my-auto border border-deeper-blue">
         <div className="relative flex flex-col p-18 pt-20 rounded-t-2xl gap-6">
-          <button
-            className="absolute top-10 right-10 rounded-full hover:cursor-pointer"
-            onClick={() => {}}
-          >
-            <HiX className="w-5 h-5 text-dark-blue hover:text-steel-blue" />
-          </button>
+          <Link href="/client">
+            <button
+              className="absolute top-10 right-10 rounded-full hover:cursor-pointer"
+              onClick={() => {}}
+            >
+              <HiX className="w-5 h-5 text-dark-blue hover:text-steel-blue" />
+            </button>
+          </Link>
           <h1 className="text-4xl font-normal m-0 text-dark-blue font-dm-serif-display mb-3">
             Computer Science Capstone: Project Proposal Form
           </h1>
@@ -244,6 +250,7 @@ export default function Form() {
                   name="ProjectTitle"
                   type="text"
                   placeholder="Enter the project title"
+                  defaultValue={projectName}
                 />
               </li>
               <li>
--- src/components/Composite/CreateProjectCard/CreateProjectCard.tsx
+++ src/components/Composite/CreateProjectCard/CreateProjectCard.tsx
@@ -1,23 +1,35 @@
+'use client'
+
 import React from 'react'
 import Input from '@/components/Generic/Input/InputField'
 import Button from '@/components/Generic/Button/Button'
+import { useState } from 'react'
+import Link from 'next/link'
 
 const CreateProjectCard = () => {
+  const [projectName, setProjectName] = useState('')
+
   return (
     <div className="w-full bg-light-beige rounded-2xl ring-1 ring-deeper-blue p-8 pt-10 pb-14">
       <h2 className="text-dark-blue font-inter text-xl tracking-wide pb-6">Create new project</h2>
       <p className="text-dark-blue font-medium text-sm pb-4">Project Name</p>
       <div className="flex flex-row justify-start gap-3">
         <div className="flex-grow">
-          <Input placeholder="Enter name" />
+          <Input
+            placeholder="Enter name"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+          />
         </div>
-        <Button
-          variant="custom"
-          size="custom"
-          className="bg-deeper-blue text-light-beige ring-1 ring-steel-blue hover:bg-steel-blue rounded-xl px-3 py-2 text-xs sm:px-4 sm:py-2 sm:text-sm"
-        >
-          Next
-        </Button>
+        <Link href={projectName === '' ? '/form' : `/form?projectName=${projectName}`}>
+          <Button
+            variant="custom"
+            size="custom"
+            className="bg-deeper-blue text-light-beige ring-1 ring-steel-blue hover:bg-steel-blue rounded-xl px-3 py-2 text-xs sm:px-4 sm:py-2 sm:text-sm"
+          >
+            Next
+          </Button>
+        </Link>
       </div>
     </div>
   )
