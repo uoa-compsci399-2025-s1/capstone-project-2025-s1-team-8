4ae37428aabf0fd19f0fa7049613f6886ba15fe3
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-30 11:47:33 +1200

    Add initial logic to test

--- src/app/(frontend)/auth/login/page.tsx
+++ src/app/(frontend)/auth/login/page.tsx
@@ -5,24 +5,48 @@ import Input from '@/components/Generic/Input/InputField'
 import { MdOutlineMail, MdLock } from 'react-icons/md'
 import { FcGoogle } from 'react-icons/fc'
 import Link from 'next/link'
+import { FormEvent, useState } from 'react'
+import UserService from '@/lib/services/userService/UserService'
+import { useRouter } from 'next/navigation'
 
 export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    const res = await UserService.login({ email, password })
+    if (res.status === 200) {
+      console.log('Login successful:', res.message)
+      router.push("/")
+
+    } else {
+      console.error('Login failed:', res.error || res.details)
+    }
+    
+  }
+
   return (
     <div className="flex flex-col justify-center items-start gap-4">
       <p className="text-xl font-extrabold text-dark-blue pl-2">WELCOME BACK</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4">
           <Input
             placeholder="Email Address"
             type="email"
             startIcon={<MdOutlineMail className="text-muted-blue h-full" />}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             placeholder="Password"
             type="password"
             startIcon={<MdLock className="text-muted-blue h-full" />}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <Button size="md" variant="dark">
+          <Button size="md" variant="dark" type="submit">
             <p className="text-xs">Login</p>
           </Button>
           <p className="text-steel-blue text-xs text-center">
