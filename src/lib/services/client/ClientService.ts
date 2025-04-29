2db451426b049d2161ba3f268bb57a3b99d0854f
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-30 11:22:06 +1200

    fix: prettier

--- src/lib/services/client/ClientService.ts
+++ src/lib/services/client/ClientService.ts
@@ -1,38 +1,36 @@
-import { GET as GetClientInfo } from "@/app/api/users/me/route"
-import {GET as GetClientProjects} from "@/app/api/users/me/projects/route"
-import { UserCombinedInfo } from "@/types/Collections"
-import { Project } from "@/payload-types"
-import { buildNextRequest } from "@/utils/buildNextRequest"
-import { buildNextRequestURL } from "@/utils/buildNextRequestURL"
+import { GET as GetClientInfo } from '@/app/api/users/me/route'
+import { GET as GetClientProjects } from '@/app/api/users/me/projects/route'
+import { UserCombinedInfo } from '@/types/Collections'
+import { Project } from '@/payload-types'
+import { buildNextRequest } from '@/utils/buildNextRequest'
+import { buildNextRequestURL } from '@/utils/buildNextRequestURL'
 
 const ClientService = {
-    /** 
-    * Fetches the client information of the currently logged-in user.
-    * @returns The client information of the user.
-    * */
-    async getClientInfo(): Promise<UserCombinedInfo> {
-        'use server'
-        const url = await buildNextRequestURL('/api/users/me', {})
-        const response = await GetClientInfo(await buildNextRequest(url, { method: 'GET' }))
-        const userInfo = await response.json()
-        return userInfo
-    },
+  /**
+   * Fetches the client information of the currently logged-in user.
+   * @returns The client information of the user.
+   * */
+  async getClientInfo(): Promise<UserCombinedInfo> {
+    'use server'
+    const url = await buildNextRequestURL('/api/users/me', {})
+    const response = await GetClientInfo(await buildNextRequest(url, { method: 'GET' }))
+    const userInfo = await response.json()
+    return userInfo
+  },
 
-    /**
-     * Fetches projects for a client.
-     * @returns The projects of the currently logged-in user.
-     */
-    async getClientProjects(): Promise<Project []> {
-        'use server'
-        const url = await buildNextRequestURL("api/users/me/projects", {})
-        const response = await GetClientProjects(await buildNextRequest(url, { method: 'GET' }))
-        const projects = await response.json()
-        return projects
-    },
+  /**
+   * Fetches projects for a client.
+   * @returns The projects of the currently logged-in user.
+   */
+  async getClientProjects(): Promise<Project[]> {
+    'use server'
+    const url = await buildNextRequestURL('api/users/me/projects', {})
+    const response = await GetClientProjects(await buildNextRequest(url, { method: 'GET' }))
+    const projects = await response.json()
+    return projects
+  },
 
-    async updateClientDetails(){
-
-    }
+  async updateClientDetails() {},
 } as const
 
 export default ClientService
