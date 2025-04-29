531f605d1cbed8d24e802983ca886152b81f1ad7
Sheena Lin <ensheanlin@gmail.com>
2025-04-29 21:12:13 +1200

    feat: UserService register created

--- src/lib/services/userService/UserService.ts
+++ src/lib/services/userService/UserService.ts
@@ -3,6 +3,7 @@ import { buildNextRequestURL } from '@/utils/buildNextRequestURL'
 import { buildNextRequest } from '@/utils/buildNextRequest'
 import { StatusCodes } from 'http-status-codes'
 import { typeToFlattenedError } from 'zod'
+import { RegisterRequestBodySchema } from '@/app/api/auth/register/route'
 
 const UserService = {
   login: async function (options: { email: string; password: string }): Promise<{
@@ -11,7 +12,19 @@ const UserService = {
     error?: string
     details?: typeToFlattenedError<typeof LoginRequestBodySchema>
   }> {
-    const url = buildNextRequestURL('/api/auth/userService', options)
+    const url = buildNextRequestURL('/api/auth/login', options)
+    const response = await POST(await buildNextRequest(url, { method: 'POST', body: options }))
+    const { message, status, error, details } = await response.json()
+    return { message, status, error, details }
+  },
+
+  register: async function(options: { email: string; password: string }): Promise<{
+    message: string
+    status: StatusCodes
+    error?: string
+    details?: typeToFlattenedError<typeof RegisterRequestBodySchema>
+  }> {
+    const url = buildNextRequestURL('/api/auth/register', options)
     const response = await POST(await buildNextRequest(url, { method: 'POST', body: options }))
     const { message, status, error, details } = await response.json()
     return { message, status, error, details }
