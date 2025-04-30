bc4158768186df7fa2f3e072c5e4192ca864dc77
Sheena Lin <ensheanlin@gmail.com>
2025-04-30 12:55:52 +1200

    changes made

--- src/lib/services/userService/UserService.ts
+++ src/lib/services/userService/UserService.ts
@@ -1,9 +1,10 @@
-import { LoginRequestBodySchema, POST } from '@/app/api/auth/login/route'
+import { LoginRequestBodySchema, POST as LoginPost } from '@/app/api/auth/login/route'
 import { buildNextRequestURL } from '@/utils/buildNextRequestURL'
 import { buildNextRequest } from '@/utils/buildNextRequest'
 import { StatusCodes } from 'http-status-codes'
 import { typeToFlattenedError } from 'zod'
-import { RegisterRequestBodySchema } from '@/app/api/auth/register/route'
+import { RegisterRequestBodySchema, POST as RegisterPost } from '@/app/api/auth/register/route'
+import { UserRole } from '@/types/User'
 
 const UserService = {
   login: async function (options: { email: string; password: string }): Promise<{
@@ -13,21 +14,29 @@ const UserService = {
     details?: typeToFlattenedError<typeof LoginRequestBodySchema>
   }> {
     const url = buildNextRequestURL('/api/auth/login', options)
-    const response = await POST(await buildNextRequest(url, { method: 'POST', body: options }))
+    const response = await LoginPost(await buildNextRequest(url, { method: 'POST', body: options }))
     const { message, status, error, details } = await response.json()
     return { message, status, error, details }
   },
 
-  register: async function (options: { email: string; password: string }): Promise<{
+  register: async function (options: {
+    email: string
+    password: string
+    firstName: string
+    lastName: string
+    Role: UserRole
+  }): Promise<{
     message: string
     status: StatusCodes
     error?: string
     details?: typeToFlattenedError<typeof RegisterRequestBodySchema>
   }> {
     const url = buildNextRequestURL('/api/auth/register', options)
-    const response = await POST(await buildNextRequest(url, { method: 'POST', body: options }))
-    const { message, status, error, details } = await response.json()
-    return { message, status, error, details }
+    const response = await RegisterPost(
+      await buildNextRequest(url, { method: 'POST', body: options }),
+    )
+    const { message, error, details } = await response.json()
+    return { message, status: response.status, error, details }
   },
 }
 
