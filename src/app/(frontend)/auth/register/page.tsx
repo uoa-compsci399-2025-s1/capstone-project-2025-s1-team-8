3a846398da366599ff49ec829711f90223e783ad
joohuil <99111909+joohuil@users.noreply.github.com>
2025-04-30 13:50:25 +1200

    feat: button links to somewhere (#236)

--- src/app/(frontend)/auth/register/page.tsx
+++ src/app/(frontend)/auth/register/page.tsx
@@ -33,7 +33,9 @@ export default function SignUp() {
             startIcon={<MdLock className="text-muted-blue h-full" />}
           />
           <Button size="md" variant="dark">
-            <p className="text-xs">Register</p>
+            <Link href="/auth/login">
+              <p className="text-xs">Register</p>
+            </Link>
           </Button>
           <p className="text-steel-blue text-xs text-center pb-4">
             {'Already have an account? Log in '}
