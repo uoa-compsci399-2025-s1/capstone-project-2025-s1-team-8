dbe1a1464800de9005a1a447ebce86aaf05e9bf8
joohuil <99111909+joohuil@users.noreply.github.com>
2025-05-06 12:36:57 +1200

    feat: pagination and status

--- src/lib/services/admin/AdminClientService.ts
+++ src/lib/services/admin/AdminClientService.ts
@@ -9,32 +9,49 @@ import { buildNextRequest } from '@/utils/buildNextRequest'
 import { typeToFlattenedError } from 'zod'
 import { UpdateUserRequestBody } from '@/app/api/admin/users/[id]/route'
 import { ClientCombinedInfo } from '@/types/Payload'
+import { StatusCodes } from 'http-status-codes'
+import { UserRole } from '@/types/User'
 
 const AdminClientService = {
-  getAllUsers: async function (): Promise<{ data: (typeof ClientCombinedInfo)[]; error?: string }> {
+  getAllUsers: async function (
+    options: { limit?: number; cursor?: number } = {},
+  ): Promise<{ 
+    status: StatusCodes
+    data?: (typeof ClientCombinedInfo)[]
+    nextPage?: string
+    error?: string 
+    userRole?: UserRole
+  }> {
     'use server'
-    const url = buildNextRequestURL('/api/admin/users', {})
+    const url = buildNextRequestURL('/api/admin/users', options)
     const response = await GetUsers(await buildNextRequest(url, { method: 'GET' }))
-    const { data, error } = { ...(await response.json()) }
-    return { data, error }
+    const { data, nextPage, error } = { ...(await response.json()) }
+    
+    return { status: response.status, data, nextPage, error }
   },
 
   getUserById: async function (
     userId: string,
-  ): Promise<{ data: typeof ClientCombinedInfo; error?: string }> {
+  ): Promise<{ 
+    status: StatusCodes
+    data: typeof ClientCombinedInfo
+    error?: string 
+  }> {
     'use server'
     const url = buildNextRequestURL(`/api/admin/users/${userId}`, {})
     const response = await GetUserById(await buildNextRequest(url, { method: 'GET' }), {
       params: Promise.resolve({ id: userId }),
     })
     const { data, error } = { ...(await response.json()) }
-    return { data, error }
+
+    return { status: response.status, data, error }
   },
 
   updateUser: async function (
     userId: string,
     user: UpdateUserRequestBody,
   ): Promise<{
+    status: StatusCodes
     data: typeof ClientCombinedInfo
     error?: string
     details?: typeToFlattenedError<UpdateUserRequestBody>
@@ -48,10 +65,12 @@ const AdminClientService = {
       },
     )
     const { data, error, details } = await response.json()
-    return { data, error, details }
+    
+    return { status: response.status, data, error, details }
   },
 
   deleteUser: async function (userId: string): Promise<{
+    status: StatusCodes
     error?: string
   }> {
     'use server'
@@ -60,19 +79,27 @@ const AdminClientService = {
       params: Promise.resolve({ id: userId }),
     })
     const { error } = await response.json()
-    return { error }
+    
+    return { status: response.status, error }
   },
 
   getProjectsByUserId: async function (
     userId: string,
-  ): Promise<{ data: Project[]; error?: string }> {
+    options: { page?: number; limit?: number } = {},
+  ): Promise<{ 
+    status: StatusCodes
+    data: Project[]
+    nextPage?: string
+    error?: string 
+  }> {
     'use server'
-    const url = buildNextRequestURL(`/api/admin/users/${userId}/projects`, {})
+    const url = buildNextRequestURL(`/api/admin/users/${userId}/projects`, options)
     const response = await GetUserProjects(await buildNextRequest(url, { method: 'GET' }), {
       params: Promise.resolve({ id: userId }),
     })
-    const { data, error } = { ...(await response.json()) }
-    return { data, error }
+    const { data, nextPage, error } = { ...(await response.json()) }
+    
+    return { status: response.status, data, nextPage, error }
   },
 } as const
 export default AdminClientService
