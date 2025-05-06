f34888d3c1e7441b5dfdb29b4640bf21ff04f3bc
Sheena Lin <ensheanlin@gmail.com>
2025-05-07 10:06:16 +1200

    fix: design

--- src/app/(frontend)/auth/register/page.tsx
+++ src/app/(frontend)/auth/register/page.tsx
@@ -1,50 +1,11 @@
 'use client'
 
-import Button from '@/components/Generic/Button/Button'
-import Input from '@/components/Generic/Input/InputField'
-import { MdOutlineMail, MdLock } from 'react-icons/md'
-import { BsFillPersonFill } from 'react-icons/bs'
-import Link from 'next/link'
+import RegisterPage from '@/components/Pages/RegisterPage/RegisterPage'
 
 export default function SignUp() {
   return (
-    <div className="flex flex-col justify-center items-start gap-4">
-      <p className="text-xl font-extrabold text-dark-blue pl-2">KIA ORA</p>
-      <form>
-        <div className="flex flex-col gap-4">
-          <Input
-            placeholder="First Name"
-            type="text"
-            startIcon={<BsFillPersonFill className="text-muted-blue h-full" />}
-          />
-          <Input
-            placeholder="Last Name"
-            type="text"
-            startIcon={<BsFillPersonFill className="text-muted-blue h-full" />}
-          />
-          <Input
-            placeholder="Email Address"
-            type="email"
-            startIcon={<MdOutlineMail className="text-muted-blue h-full" />}
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            startIcon={<MdLock className="text-muted-blue h-full" />}
-          />
-          <Button size="md" variant="dark">
-            <Link href="/auth/login">
-              <p className="text-xs">Register</p>
-            </Link>
-          </Button>
-          <p className="text-steel-blue text-xs text-center pb-4">
-            {'Already have an account? Log in '}
-            <Link href="/auth/login">
-              <u>here</u>
-            </Link>
-          </p>
-        </div>
-      </form>
+    <div>
+      <RegisterPage />
     </div>
   )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,13 +9,15 @@ export default function NotFound() {
     <div className="h-dvh flex flex-col items-center space-y-8 bg-[linear-gradient(to_right,rgba(255,169,222,0.2),rgba(209,251,255,0.2)),linear-gradient(to_bottom,#fffef9,#D1FBFF)]">
       <NavBar />
       <div className="flex flex-1 flex-col justify-center items-center space-y-8 p-10">
-        <div className="flex items-end">
-          <Image src="/error.svg" alt="error" width={160} height={160} priority />
-          <p className="text-error-color text-7xl font-silkscreen pb-4">ERROR</p>
+        <div className="flex items-end mb-10 mr-25">
+          <Image src="/error.svg" alt="error" width={192} height={192} priority />
+          <p className="text-deeper-blue text-[5.25rem] font-silkscreen pb-15 pl-5">ERROR</p>
         </div>
-        <Button variant="outline" size="md">
-          <Link href="/">Return to home</Link>
-        </Button>
+        <Link href="/public">
+          <Button variant="outline" size="md">
+            Return to home
+          </Button>
+        </Link>
       </div>
     </div>
   )
