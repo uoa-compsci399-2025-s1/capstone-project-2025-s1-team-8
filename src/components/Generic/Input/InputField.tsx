d13a0ba3e30570536421a2c71876cd15bbc0bcc4
Sheena Lin <ensheanlin@gmail.com>
2025-05-06 12:17:10 +1200

    fix: design

--- src/components/Generic/Input/InputField.tsx
+++ src/components/Generic/Input/InputField.tsx
@@ -78,7 +78,7 @@ const Input: FC<InputProps> = ({
         ) : null}
       </div>
       {error && (
-        <div className="flex items-center gap-2 text-xs text-pink-accent min-h-[1.25rem] mt-2">
+        <div className="flex items-center gap-2 text-xs text-pink-accent min-h-[1.25rem]">
           <ExclamationTriangleIcon className="w-3 h-3" />
           <p>{errorMessage}</p>
         </div>
--- src/components/Pages/RegisterPage/RegisterPage.tsx
+++ src/components/Pages/RegisterPage/RegisterPage.tsx
@@ -72,23 +72,23 @@ const Register = () => {
               </Link>
             </p>
           </div>
-        </div>
-        <div className="flex items-center w-full pt-2 pb-3">
-          <div className="border-t border-steel-blue flex-grow"></div>
-          <span className="mx-4 text-steel-blue text-xs">OR</span>
-          <div className="border-t border-steel-blue flex-grow"></div>
-        </div>
-        <div className="w-full">
-          <Link href="/api/auth/google">
-            <Button
-              className="bg-white w-full"
-              size="md"
-              variant="outline"
-              startIcon={<FcGoogle className="h-full" />}
-            >
-              <p className="text-xs text-center pt-0.5">Sign Up with Google</p>
-            </Button>
-          </Link>
+          <div className="flex items-center w-full pt-2 pb-3">
+            <div className="border-t border-steel-blue flex-grow"></div>
+            <span className="mx-4 text-steel-blue text-xs">OR</span>
+            <div className="border-t border-steel-blue flex-grow"></div>
+          </div>
+          <div className="w-full">
+            <Link href="/api/auth/google">
+              <Button
+                className="bg-white w-full"
+                size="md"
+                variant="outline"
+                startIcon={<FcGoogle className="h-full" />}
+              >
+                <p className="text-xs text-center pt-0.5">Sign Up with Google</p>
+              </Button>
+            </Link>
+          </div>
         </div>
       </form>
     </div>
