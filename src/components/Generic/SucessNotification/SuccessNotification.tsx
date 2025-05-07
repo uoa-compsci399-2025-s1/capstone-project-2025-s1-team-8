75fedf8d0c988b89e6fa96decf0d06a2c6e82a03
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-05-07 21:02:33 +1200

    fix: prettier

--- src/components/Generic/SucessNotification/SuccessNotification.tsx
+++ src/components/Generic/SucessNotification/SuccessNotification.tsx
@@ -2,22 +2,26 @@ import { FC } from 'react'
 import { FiAlertCircle } from 'react-icons/fi'
 
 interface SuccessNotificationProps {
-  isVisible: boolean;
-  title: string;
-  message: string;
+  isVisible: boolean
+  title: string
+  message: string
 }
 
-const SuccessNotification: FC<SuccessNotificationProps> = ({isVisible=true, title, message}) => {
+const SuccessNotification: FC<SuccessNotificationProps> = ({
+  isVisible = true,
+  title,
+  message,
+}) => {
   return (
-    <div className={` ${isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-all duration-500 z-50 bg-light-green ring ring-2 ring-green-soft shadow-md rounded-lg px-6 py-4 max-w-md flex flex-col`}>
+    <div
+      className={` ${isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-all duration-500 z-50 bg-light-green ring ring-2 ring-green-soft shadow-md rounded-lg px-6 py-4 max-w-md flex flex-col`}
+    >
       <div className="flex items-center gap-2">
         <FiAlertCircle className="text-green-accent w-5 h-5 flex-shrink-0" />
         <p className="text-dark-green font-medium">{title}</p>
       </div>
 
-      <p className="text-dark-green text-sm">
-        {message}
-      </p>
+      <p className="text-dark-green text-sm">{message}</p>
     </div>
   )
 }
