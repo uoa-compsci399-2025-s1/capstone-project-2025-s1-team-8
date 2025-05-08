9b93bb2193c1b956529dabc74c81294ed5d8b91c
bethany-aroha <126203114+bethany-aroha@users.noreply.github.com>
2025-05-08 20:02:19 +1200

    fix: styling changes

--- src/components/Generic/RadialMenuItems/RadialMenuItem.tsx
+++ src/components/Generic/RadialMenuItems/RadialMenuItem.tsx
@@ -4,12 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useHover, useLayer } from 'react-laag'
 import React from 'react'
 
-function getTransform(
-  progress: number,
-  radius: number,
-  index: number,
-  totalItems: number,
-): string {
+function getTransform(progress: number, radius: number, index: number, totalItems: number): string {
   const angle = (index / (totalItems + 5)) * 2 * Math.PI - Math.PI / 2 // Start at top
   const animatedRadius = radius * progress
   const x = -animatedRadius * Math.cos(angle)
@@ -26,14 +21,20 @@ type RadialMenuItemProps = {
   onClick: () => void
 }
 
-const RadialMenuItem: React.FC<RadialMenuItemProps> = ({ Icon, label, index, totalItems, onClick }) => {
+const RadialMenuItem: React.FC<RadialMenuItemProps> = ({
+  Icon,
+  label,
+  index,
+  totalItems,
+  onClick,
+}) => {
   const [isHovering, hoverProps] = useHover({ delayEnter: 300, delayLeave: 100 })
 
   const { triggerProps, layerProps, renderLayer } = useLayer({
     isOpen: isHovering,
     auto: true,
     ResizeObserver,
-  });
+  })
 
   return (
     <div className="absolute">
