99c85f4f45d07f5eb540c9881563893976bfd385
Sheena Lin <ensheanlin@gmail.com>
2025-05-08 17:10:56 +1200

    component created

---
+++ src/app/(frontend)/admin/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../globals.css'
+import AdminMobileView from './adminMobileView'
+
+export const metadata = {
+  description: 'A blank template using Payload in a Next.js app.',
+  title: 'Payload Blank Template',
+}
+
+export default async function AdminLayout(props: { children: React.ReactNode }) {
+  const { children } = props
+
+  return (
+    <html lang="en">
+    <body>
+    {/*<main>{children}</main>*/}
+    <AdminMobileView/>
+    </body>
+    </html>
+  )
+}
