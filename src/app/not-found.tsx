43fdf48eb2d29788ee3b0a81f37bc22cfe15c15a
joohuil <99111909+joohuil@users.noreply.github.com>
2025-05-08 20:35:10 +1200

    feat: mobile view

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,9 +9,9 @@ export default function NotFound() {
     <div className="overflow-x-auto min-h-screen flex flex-col items-center space-y-8 bg-[linear-gradient(to_right,rgba(255,169,222,0.2),rgba(209,251,255,0.2)),linear-gradient(to_bottom,#fffef9,#D1FBFF)]">
       <NavBar hasBg={false} />
       <div className="flex flex-1 flex-col justify-center items-center space-y-8 p-10">
-        <div className="flex items-end my-10 mx-4 sm:mx-10 md:mx-25">
+        <div className="flex flex-col sm:flex-row items-end my-10 mx-4 sm:mx-10 md:mx-25">
           <Error />
-          <p className="text-deeper-blue text-[5.25rem] font-silkscreen pb-15 pl-5">ERROR</p>
+          <p className="text-deeper-blue text-5xl sm:text-[5.25rem] font-silkscreen sm:pb-8 sm:pl-5">ERROR</p>
         </div>
         <Link href="/">
           <Button variant="outline" size="md">
