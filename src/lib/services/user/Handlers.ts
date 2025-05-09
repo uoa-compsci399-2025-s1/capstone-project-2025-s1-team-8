67d63a607bb4487bc7da4ffd974305621650320a
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-09 22:42:20 +1200

    303 Fix user service to use correct folder (#304)
    * refactor: delete userService to replace functions into user service
    
    - Renamed POST imports for consistency (e.g., LoginPost -> Login).
    - Added `getUserSelfData` method to fetch user data.
    - Added `logout` method to handle user logout.
    - Removed deprecated `userService/UserService.ts` file.
    - Updated references to use the new `UserService` structure.
    
    * refactor: improve email validation and add unit tests
    
    * refactor: login utilities into dedicated Handlers module
    
    - Moved `handleLogin`, `isLoggedIn`, and `handleLoginButtonClick`
      to `src/lib/services/user/Handlers.ts`.
    - Updated imports in affected files to reflect the new module path.
    
    * test: cases for invalid email formats with special characters
    
    * refactor: email validation case-insensitive
    
    Updated regex to include the 'i' flag for case-insensitivity.
    Added a test case to verify behavior with mixed-case emails.

---
+++ src/lib/services/user/Handlers.ts
@@ -0,0 +1,60 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+import { typeToFlattenedError } from 'zod'
+
+import { LoginRequestBodySchema } from '@/app/api/auth/login/route'
+import UserService from './UserService'
+import { isValidEmail } from '@/lib/util/util'
+
+/**
+ * This function handles the form submission for user login.
+ * It validates the input data and calls the UserService to perform the login action.
+ * @param formData - The form data containing email and password.
+ * @returns an object containing error messages or redirects to the appropriate page.
+ */
+export async function handleLogin(formData: FormData): Promise<{
+  error?: string
+  message?: string
+  redirect?: string
+  details?: typeToFlattenedError<typeof LoginRequestBodySchema>
+}> {
+  const email = formData.get('email') as string
+  const password = formData.get('password') as string
+
+  if (!email && !password) return { error: 'Email and password are required' }
+  if (!email) return { error: 'Email is required' }
+  if (!password) return { error: 'Password is required' }
+  if (!isValidEmail(email)) return { error: 'Invalid email address format' }
+
+  const { message, redirect, status, error, details } = await UserService.login({ email, password })
+  if (status === 200) {
+    return { message, redirect }
+  } else {
+    return { error, message, details }
+  }
+}
+
+/**
+ * Checks if user is logged in
+ *
+ * @returns a boolean indicating whether the user is logged in or not.
+ */
+export async function isLoggedIn(): Promise<boolean> {
+  const res = await UserService.getUserSelfData()
+  return res.status === 200
+}
+
+/**
+ * Handles the click event for login/logout button
+ * Redirects to the appropriate page based on the login status.
+ */
+export async function handleLoginButtonClick() {
+  const status = await isLoggedIn()
+  if (status) {
+    await UserService.logout()
+    redirect('/')
+  } else {
+    redirect('/auth/login')
+  }
+}
