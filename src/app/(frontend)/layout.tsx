997b5c5d809c660af580b7f057fae33456c61fb6
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-09 15:17:38 +1200

    fix: add global styles and frontend layout components
    - Introduced `globals.css` with base, component, and utility styles.
    - Added `layout.tsx` for the frontend with metadata and basic structure.
    - Updated root `layout.tsx` to simplify HTML structure.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -10,10 +10,6 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
-    <body>
-      {children}
-    </body>
-  </html>
+    <div>{children}</div>
   )
 }
