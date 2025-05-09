eb37c6181ea5696f14bd87e707afe6c0053df924
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-09 22:12:16 +1200

    refactor: improve email validation and add unit tests

---
+++ src/lib/util/util.test.ts
@@ -0,0 +1,24 @@
+import { isValidEmail } from './util'
+
+describe('lib/utils', () => {
+  describe('isValidEmail', () => {
+    it('should catch correct emails', () => {
+      expect(isValidEmail('jeffery@gmail.com')).toBeTruthy()
+      expect(isValidEmail('je.ffery@x.com')).toBeTruthy()
+      expect(isValidEmail('jeffe.ry@gmail.submail.com')).toBeTruthy()
+      expect(isValidEmail('j@m.com')).toBeTruthy()
+    })
+
+    it('should catch incorrect emails', () => {
+      expect(isValidEmail('je.ffery@x..com')).toBeFalsy()
+      expect(isValidEmail('@1.com')).toBeFalsy()
+      expect(isValidEmail('1@.com')).toBeFalsy()
+      expect(isValidEmail('1@.')).toBeFalsy()
+      expect(isValidEmail('1@x.')).toBeFalsy()
+      expect(isValidEmail('@x.com')).toBeFalsy()
+      expect(isValidEmail('@x')).toBeFalsy()
+      expect(isValidEmail('@')).toBeFalsy()
+      expect(isValidEmail('obviouslyfakeemail')).toBeFalsy()
+    })
+  })
+})
