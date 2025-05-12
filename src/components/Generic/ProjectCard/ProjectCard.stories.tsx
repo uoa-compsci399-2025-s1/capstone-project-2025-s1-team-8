d99dfb4b6c421be0280ee78583cc2f71fd2ebd58
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-05-13 02:00:47 +1200

    refactor story types

--- src/components/Generic/ProjectCard/ProjectCard.stories.tsx
+++ src/components/Generic/ProjectCard/ProjectCard.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import ProjectCard from './ProjectCard'
-import { mockProjects } from '@/mocks/Projects.mock'
+import { mockProjects } from '@/mocks/newProjects.mock'
 
 const meta: Meta<typeof ProjectCard> = {
   component: ProjectCard,
