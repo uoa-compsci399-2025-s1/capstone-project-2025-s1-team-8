80dee3d7ace1cd7e8b46ad25d70ba0ecc5af0ef3
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-05-14 10:04:49 +1200

    324 refactor refactor navbar to take in user as a prop (#327)
    * Refactor navbar to take in user object
    
    * fix: prettier
    
    * fix: prettier
    
    * Update story!
    
    * Update NavBar.stories.tsx
    
    * Add extra link for client dashboard on admin page
    
    * Fix register page redirect error
    
    * Add Client Dashboard link to navbar
    
    * Update NavBar.tsx
    
    * Update NavBar.tsx
    
    * fix prettier
    
    * prettier
    
    * remove comments
    
    * redirect if user is not admin
    
    * fix home page always re rendering

--- src/app/(frontend)/client/page.tsx
+++ src/app/(frontend)/client/page.tsx
@@ -5,7 +5,7 @@ import ClientDashboard from '@/components/Pages/ClientDashboard/ClientDashboard'
 import { useState, useEffect } from 'react'
 import { handleClientPageLoad } from '@/lib/services/client/Handlers'
 import { UserCombinedInfo } from '@/types/Collections'
-import { handleLoginButtonClick, isLoggedIn } from '@/lib/services/user/Handlers'
+import { handleLoginButtonClick } from '@/lib/services/user/Handlers'
 import { handleClientProfileUpdate } from '@/lib/services/client/Handlers'
 import { ProjectDetails } from '@/types/Project'
 
@@ -13,8 +13,6 @@ export default function Client() {
   const [isLoaded, setIsLoaded] = useState<boolean>(false)
   const [user, setUser] = useState<UserCombinedInfo>({} as UserCombinedInfo)
   const [projects, setProjects] = useState<ProjectDetails[]>([])
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
-  const [loginLoaded, setLoginLoaded] = useState<boolean>(false)
 
   useEffect(() => {
     handleClientPageLoad().then((res) => {
@@ -22,24 +20,16 @@ export default function Client() {
       setProjects(res.projects)
       setIsLoaded(true)
     })
-    isLoggedIn().then((res) => {
-      setLoggedIn(res)
-      setLoginLoaded(true)
-    })
   }, [])
 
-  if (!isLoaded || !loginLoaded) {
+  if (!isLoaded) {
     return null
   }
 
   return (
     <div className="flex justify-center items-center">
       <div>
-        <NavBar
-          navElements={[{ href: '/client', text: 'My Dashboard' }]}
-          loggedIn={loggedIn}
-          onclick={handleLoginButtonClick}
-        />
+        <NavBar user={user} onclick={handleLoginButtonClick} />
       </div>
       <div className="items-center justify-center w-full px-8 sm:px-15 lg:px-30 pt-40 pb-20">
         <ClientDashboard client={user} projects={projects} onSave={handleClientProfileUpdate} />
