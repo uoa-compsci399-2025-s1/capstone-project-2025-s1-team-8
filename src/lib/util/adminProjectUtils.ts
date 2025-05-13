dbf6e1ea6d964dcd90ed783563bdad1b726bd901
midnightcuberx <drcubing27@gmail.com>
2025-05-14 11:59:42 +1200

    fix: prettier

--- src/lib/util/adminProjectUtils.ts
+++ src/lib/util/adminProjectUtils.ts
@@ -1,11 +1,11 @@
 'use server'
 
-import { DndComponentProps } from "@/components/Composite/ProjectDragAndDrop/ProjectDnD";
-import AdminProjectService from "../services/admin/AdminProjectService";
+import { DndComponentProps } from '@/components/Composite/ProjectDragAndDrop/ProjectDnD'
+import AdminProjectService from '../services/admin/AdminProjectService'
 
 export const getNextSemesterProjects = async (): Promise<void | {
-    data?: DndComponentProps
-  }> => {
-    const response = await AdminProjectService.getNextSemesterProjects();
-    return {data: response.data}  // Assuming response.projects is an array of DndComponentProps[]
+  data?: DndComponentProps
+}> => {
+  const response = await AdminProjectService.getNextSemesterProjects()
+  return { data: response.data } // Assuming response.projects is an array of DndComponentProps[]
 }
