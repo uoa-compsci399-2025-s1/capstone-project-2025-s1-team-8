b39458654d97e887fdd97c3b1ac86f5e222b9215
Sheena Lin <ensheanlin@gmail.com>
2025-05-14 21:15:22 +1200

    semester notification fix

--- src/app/(frontend)/admin/page.tsx
+++ src/app/(frontend)/admin/page.tsx
@@ -10,13 +10,13 @@ import { mockProjects } from '@/test-config/mocks/Project.mock'
 import { handleLoginButtonClick, getLoggedInUser } from '@/lib/services/user/Handlers'
 import { getAllSemesters } from '@/lib/util/adminSemesterUtils'
 import { Semester } from '@/payload-types'
-import { FiAlertCircle } from 'react-icons/fi'
 import MobileAdminView from '@/app/(frontend)/admin/MobileAdminView'
 import { UserCombinedInfo } from '@/types/Collections'
 import { redirect } from 'next/navigation'
 import { UserRole } from '@/types/User'
 import { getAllClients } from '@/lib/util/adminClientUtils'
 import { ProjectDetails } from '@/types/Project'
+import Notification from '@/components/Generic/Notification/Notification'
 
 const Admin = () => {
   const AdminNavElements = ['Projects', 'Clients', 'Semesters']
@@ -26,9 +26,8 @@ const Admin = () => {
   const [loginLoaded, setLoginLoaded] = useState<boolean>(false)
   const [semestersData, setSemestersData] = useState<Semester[]>([])
   const [showNotification, setShowNotification] = useState<boolean>(false)
-  const [created, setCreated] = useState(false)
-  const [updated, setUpdated] = useState(false)
-  const [deleted, setDeleted] = useState(false)
+  const [notificationMessage, setNotificationMessage] = useState('')
+
   const [clientsData, setClientsData] = useState<
     { client: UserCombinedInfo; projects: ProjectDetails[] }[]
   >([])
@@ -46,14 +45,6 @@ const Admin = () => {
       setClientsData(res.data)
     }
   }
-
-  const message = created
-    ? 'Semester created successfully'
-    : updated
-      ? 'Semester updated successfully'
-      : deleted
-        ? 'Semester deleted successfully'
-        : ''
 
   useEffect(() => {
     fetchSemesters()
@@ -138,15 +129,14 @@ const Admin = () => {
     <div>
       <NavBar onclick={handleLoginButtonClick} user={loggedInUser} />
       <div className="hidden lg:block w-full">
+        <div className="fixed top-6 right-6 z-50">
+          <Notification
+            isVisible={showNotification}
+            title={'Success'}
+            message={notificationMessage ?? 'hello world'}
+          />
+        </div>
         <div className="mt-25 w-full flex justify-center items-center gap-25 bg-beige pb-7">
-          <div
-            className={` ${showNotification ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-all duration-500 fixed top-6 right-6 z-50 bg-light-pink ring ring-2 ring-pink-soft shadow-md rounded-lg px-6 py-4 max-w-md flex flex-col`}
-          >
-            <div className="flex items-center gap-2">
-              <FiAlertCircle className="text-pink-accent w-5 h-5 flex-shrink-0" />
-              <p className="text-dark-pink font-medium">{message}</p>
-            </div>
-          </div>
           {AdminNavElements.map((nav, i) => (
             <button
               key={nav}
@@ -202,15 +192,15 @@ const Admin = () => {
                   semesters={semestersData}
                   created={() => {
                     setShowNotification(true)
-                    setCreated(true)
+                    setNotificationMessage('Semester created successfully')
                   }}
                   updated={() => {
                     setShowNotification(true)
-                    setUpdated(true)
+                    setNotificationMessage('Semester updated successfully')
                   }}
                   deleted={() => {
                     setShowNotification(true)
-                    setDeleted(true)
+                    setNotificationMessage('Semester deleted successfully')
                   }}
                 />
               </div>
