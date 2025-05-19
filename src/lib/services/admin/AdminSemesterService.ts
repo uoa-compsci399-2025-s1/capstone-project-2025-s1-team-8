2980c5075749aa3dcc87cfb8b854bf11c623b794
Sheena Lin <ensheanlin@gmail.com>
2025-05-19 13:59:26 +1200

    update isCurrentOrUpcoming

--- src/lib/services/admin/AdminSemesterService.ts
+++ src/lib/services/admin/AdminSemesterService.ts
@@ -161,10 +161,10 @@ const AdminSemesterService = {
     const responseNext = await GetSemesters(await buildNextRequest(urlNext, { method: 'GET' }))
     const dataNext = await responseNext.json()
 
-    if (dataCurrent.data[0].id === semesterId) {
+    if (dataCurrent.data[0]?.id === semesterId) {
       return 'current'
     }
-    if (dataNext.data[0].id === semesterId) {
+    if (dataNext.data[0]?.id === semesterId) {
       return 'upcoming'
     }
     return ''
