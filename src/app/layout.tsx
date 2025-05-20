57b27f6da463d4833aaa054bea6b2acdbd5b5e10
Sheena Lin <ensheanlin@gmail.com>
2025-05-20 13:48:36 +1200

    only dark-mode favicon shows

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import '@/app/(frontend)/globals.css'
 export const metadata = {
   description: 'Connecting Computer Science Students with Innovative Projects',
   title: 'Encapsulate',
+  icons: {
+    icon: [
+      { url: '/dark-logo.png', media: '(prefers-color-scheme: light)' },
+      { url: '/light-logo.png', media: '(prefers-color-scheme: dark)' },
+    ],
+  },
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
