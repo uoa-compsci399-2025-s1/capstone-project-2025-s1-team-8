058ad8593a07e6624042c7632543a4ec088c351b
joohuil <99111909+joohuil@users.noreply.github.com>
2025-05-23 13:45:36 +1200

    371 frontend register role dropdown (#375)
    * feat: role dropdown on sign up page
    
    * fix: styling changes
    
    * feat: formData has 'role'
    
    * feat: separate google auth register by role
    
    * fix: yassify and lint
    
    * fix: remove selected on option
    
    * fix: remove 'with google' on button

--- src/app/(frontend)/auth/register/page.tsx
+++ src/app/(frontend)/auth/register/page.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/Generic/Button/Button'
 import Input from '@/components/Generic/Input/InputField'
 import { MdOutlineMail, MdLock } from 'react-icons/md'
 import { FcGoogle } from 'react-icons/fc'
+import { FaChevronDown } from 'react-icons/fa'
 import Link from 'next/link'
 import { handleRegister } from '@/lib/services/user/Handlers'
 import { useState } from 'react'
@@ -12,6 +13,7 @@ import { BsFillPersonFill } from 'react-icons/bs'
 export default function RegisterPage() {
   const [errorState, setErrorState] = useState<boolean>(false)
   const [errorMessage, setErrorMessage] = useState<string>('')
+  const [role, setRole] = useState('')
 
   const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -26,11 +28,31 @@ export default function RegisterPage() {
     }
   }
 
+  const isPlaceholder = role === ''
+
   return (
-    <div className="flex flex-col justify-center items-start gap-4">
+    <div className="flex flex-col justify-center items-start gap-4 pt-20 lg:pt-0">
       <p className="text-xl font-extrabold text-dark-blue pl-2">KIA ORA</p>
       <form onSubmit={submitForm}>
         <div className="flex flex-col gap-4 relative z-[51]">
+          <div className="relative inline-block w-full">
+            <select
+              defaultValue=""
+              name="role"
+              onChange={(e) => setRole(e.target.value)}
+              required
+              className={`appearance-none ring-muted-blue ring-1 focus:ring-3 w-full pl-11 focus:outline-hidden rounded-lg px-4 py-2.5 text-sm ${isPlaceholder ? 'text-deeper-blue bg-white' : 'text-steel-blue bg-muted-blue-op-45'}`}
+            >
+              <option value="" disabled className="text-muted-blue">
+                Role
+              </option>
+              <option value="client">Client</option>
+              <option value="student">Student</option>
+            </select>
+            <span className="absolute left-4 top-7/13 -translate-y-1/2 h-3 w-3">
+              <FaChevronDown className="text-deeper-blue h-full" />
+            </span>
+          </div>
           <Input
             name="firstName"
             placeholder="First Name"
@@ -80,15 +102,29 @@ export default function RegisterPage() {
             <div className="border-t border-steel-blue flex-grow"></div>
           </div>
         </div>
-        <div className="w-full">
-          <Link href="/api/auth/google">
+        <div className="w-full flex flex-col gap-3">
+          <Link href="/api/auth/google?role=client">
             <Button
               className="bg-white w-full"
               size="md"
               variant="outline"
               startIcon={<FcGoogle className="h-full" />}
             >
-              <p className="text-xs text-center pt-0.5">Sign Up with Google</p>
+              <p className="text-xs text-center pt-0.5">
+                Sign Up as a <b>Client</b>
+              </p>
+            </Button>
+          </Link>
+          <Link href="/api/auth/google?role=student">
+            <Button
+              className="bg-white w-full"
+              size="md"
+              variant="outline"
+              startIcon={<FcGoogle className="h-full" />}
+            >
+              <p className="text-xs text-center pt-0.5">
+                Sign Up as a <b>Student</b>
+              </p>
             </Button>
           </Link>
         </div>
