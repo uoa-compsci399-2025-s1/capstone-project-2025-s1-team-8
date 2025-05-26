34af58f38efe2c79204dfec73285f0dd100c9817
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-05-27 00:40:20 +1200

    feat: global home API endpoint with tests

---
+++ src/app/api/globals/home/route.ts
@@ -0,0 +1,10 @@
+import { payload } from '@/data-layer/adapters/Payload'
+import { NextResponse } from 'next/server'
+
+export const GET = async () => {
+  const home = await payload.findGlobal({
+    slug: 'Home',
+  })
+
+  return NextResponse.json({ data: home })
+}
