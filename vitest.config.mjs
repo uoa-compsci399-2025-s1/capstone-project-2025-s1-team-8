ee4195e91e8e2a43ffa1117029be3918f3b10a73
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-03 01:19:05 +1200

    build: update react plugin and test environment

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react({
+      tsDecorators: true,
+    }),
+  ],
   test: {
-    environment: 'node',
+    environment: 'jsdom',
     setupFiles: [
       'dotenv/config',
       './src/test-config/mongodb-setup.ts',
