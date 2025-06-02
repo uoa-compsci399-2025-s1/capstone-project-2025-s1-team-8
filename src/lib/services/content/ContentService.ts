2321985dc7cd9922ed09fa681af0b431a4511546
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-03 01:28:31 +1200

    feat(content): return fallback status for home page

--- src/lib/services/content/ContentService.ts
+++ src/lib/services/content/ContentService.ts
@@ -4,23 +4,27 @@ import homeContent from '@/lib/defaults/Home'
 
 const ContentService = {
   /**
-   * Method to fetch the home page content from Payload
+   * Fetches the home page content from the CMS.
    *
    * @returns The home page content
    */
-  getHomePage: async function (): Promise<Home> {
+  getHomePage: async function (): Promise<{ content: Home; isFallback: boolean }> {
     'use server'
     const homePage = await GetHomePage()
 
     let homePageCMS: Home = homeContent
+    let isFallback = false
+
     if (homePage.ok) {
       const json = await homePage.json()
       homePageCMS = json.data
     } else {
       console.error('Failed to fetch home page content: ', homePage.status)
+      console.warn('Using fallback content for the home page.')
+      isFallback = true
     }
 
-    return homePageCMS
+    return { content: homePageCMS, isFallback }
   },
 }
 export default ContentService
