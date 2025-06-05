e480f8870d76944ff9cbcada9b3967d109d07dd3
joohuil <99111909+joohuil@users.noreply.github.com>
2025-06-06 11:04:12 +1200

    fix: pagination styling

--- src/components/Pages/ClientsPage/ClientsPage.tsx
+++ src/components/Pages/ClientsPage/ClientsPage.tsx
@@ -83,7 +83,7 @@ const ClientsPage: React.FC<ClientsPageProps> = ({
           disabled={pageNum === 1 || isFetching}
           className={pageNum === 1 ? 'opacity-30 cursor-default' : 'cursor-pointer'}
         >
-          <MdFirstPage size="2em" />
+          <MdFirstPage size="1.5em" />
         </button>
 
         <button
@@ -91,17 +91,17 @@ const ClientsPage: React.FC<ClientsPageProps> = ({
           disabled={pageNum === 1 || isFetching}
           className={pageNum === 1 ? 'opacity-30 cursor-default' : 'cursor-pointer'}
         >
-          <MdOutlineNavigateBefore size="2em" />
+          <MdOutlineNavigateBefore size="1.5em" />
         </button>
 
-        <p>{pageNum}</p>
+        <p className="text-dark-blue">{pageNum}</p>
 
         <button
           onClick={async () => await updatePageCount(true)}
           disabled={pageNum >= totalPages || isFetching}
           className={pageNum >= totalPages ? 'opacity-30 cursor-default' : 'cursor-pointer'}
         >
-          <MdOutlineNavigateNext size="2em" />
+          <MdOutlineNavigateNext size="1.5em" />
         </button>
 
         <button
@@ -109,7 +109,7 @@ const ClientsPage: React.FC<ClientsPageProps> = ({
           disabled={pageNum >= totalPages || isFetching}
           className={pageNum >= totalPages ? 'opacity-30 cursor-default' : 'cursor-pointer'}
         >
-          <MdLastPage size="2em" />
+          <MdLastPage size="1.5em" />
         </button>
       </div>
     </div>
