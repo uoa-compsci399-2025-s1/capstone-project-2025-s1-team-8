4ea6258edfce06b2492e353d260861860bbb989e
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-06 01:01:27 +1200

    feat(clientdashboard): implement dynamic content (#456)
    * feat(home): add satisfies Home type assertion
    
    * fix(globals): fix tip content spacing
    
    * feat: add client dashboard content service
    
    * feat(clientdashboard): integrate cms content
    
    * fix: fix typo in client dashboard fetch
    
    * refactor: use type import for payload types

--- src/app/api/globals/client/route.test.ts
+++ src/app/api/globals/client/route.test.ts
@@ -10,7 +10,7 @@ describe('globals/client', () => {
       expect(json).toEqual({
         data: {
           tipContent:
-            '• Keep it short and straight to the point!\n\n• Please use the description if you would like to say more!',
+            '• Keep it short and straight to the point!\n• Please use the description if you would like to say more!',
           tipTitle: 'Tips for choosing a good project name:',
         },
       })
--- src/lib/services/content/ContentService.ts
+++ src/lib/services/content/ContentService.ts
@@ -1,6 +1,8 @@
-import type { Home } from '@/payload-types'
-import { GET as GetHomePage } from '@/app/api/globals/home/route'
+import type { ClientDashboard, Home } from '@/payload-types'
+import { GET as getHomePage } from '@/app/api/globals/home/route'
+import { GET as getClientDashboard } from '@/app/api/globals/client/route'
 import homeContent from '@/lib/defaults/Home'
+import clientDashboardContent from '@/lib/defaults/ClientDashboard'
 
 const ContentService = {
   /**
@@ -10,7 +12,7 @@ const ContentService = {
    */
   getHomePage: async function (): Promise<{ content: Home; isFallback: boolean }> {
     'use server'
-    const homePage = await GetHomePage()
+    const homePage = await getHomePage()
 
     let homePageCMS: Home = homeContent
     let isFallback = false
@@ -25,6 +27,27 @@ const ContentService = {
     }
 
     return { content: homePageCMS, isFallback }
+  },
+  getClientDashboard: async function (): Promise<{
+    content: ClientDashboard
+    isFallback: boolean
+  }> {
+    'use server'
+    const clientDashboard = await getClientDashboard()
+
+    let clientDashboardCMS: ClientDashboard = clientDashboardContent
+    let isFallback = false
+
+    if (clientDashboard.ok) {
+      const json = await clientDashboard.json()
+      clientDashboardCMS = json.data
+    } else {
+      console.error('Failed to fetch client dashboard content: ', clientDashboard.status)
+      console.warn('Using fallback content for the client dashboard.')
+      isFallback = true
+    }
+
+    return { content: clientDashboardCMS, isFallback }
   },
 }
 export default ContentService
