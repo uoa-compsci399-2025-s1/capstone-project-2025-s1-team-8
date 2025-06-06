89a817fdb29d8d4347b3c26536abade103d14fdd
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-06 13:11:54 +1200

    388 frontend update navbar to have dropdown for admins (#392)
    * Add Navdropdown
    
    * add back radix ui
    
    * Revert "add back radix ui"
    
    This reverts commit a447f4fe4e9270a4c2a8b52964d8e4e431cbe1a1.
    
    * Revert "Add Navdropdown"
    
    This reverts commit 64dbedc812779dc98fa611bf48cfdfda7d8b3805.
    
    * Add NavDropdown
    
    * prettier
    
    * Feat: Add mobile navbar
    
    * Update NavDropdown.tsx
    
    * fix: bug on small screens where there would be 2 dropdowns
    
    * Add beige background
    
    * Update NavDropdown.tsx
    
    * change about page links on the navbar
    
    * Redo navbar to not use external dependency
    
    * Adjust margin for dropdown
    
    * Update NavDropdown.tsx
    
    * Fix storybook error
    
    * Fix nitpicks
    
    Remove headless UI package
    Remove about page link from navbar
    Fix overlay issue with dropdown elements
    Increase gap between chevron icon and text
    Align nav dropdown text to the my dashboard text
    
    * Add indent and more spacing to mobile navbar
    
    * Add hover effect to my dashboard
    
    * Fix padding on dropdown
    
    * Refactor navdropdown to take in dropdownopen prop
    
    * Fix navbar centering
    
    * Update NavDropdown.tsx
    
    * Fix nitpicks
    
    * fix: prettier
    
    * Fix: nitpicks

---
+++ src/components/Generic/NavDropdown/NavDropdown.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import type { NavLink } from '../NavBar/NavBar'
+import Link from 'next/link'
+import React from 'react'
+import { IoChevronDown, IoChevronUp } from 'react-icons/io5'
+
+interface NavDropdownProps {
+  buttonText: string
+  dropdownOpen: boolean
+  setDropdownOpen: (open: boolean) => void
+  items: NavLink[]
+}
+
+const NavDropdown: React.FC<NavDropdownProps> = ({
+  buttonText,
+  items,
+  dropdownOpen = false,
+  setDropdownOpen,
+}) => {
+  const toggleMenu = () => setDropdownOpen(!dropdownOpen)
+  return (
+    <div className="relative">
+      {/* Mobile: render items inline */}
+      <div className="md:hidden text-left" onClick={toggleMenu}>
+        <div className="pb-2">
+          <button className="flex flex-row justify-between w-full nav-link-text">
+            {buttonText}
+            {!dropdownOpen && <IoChevronDown aria-hidden="true" className="size-4 text-current" />}
+            {dropdownOpen && <IoChevronUp aria-hidden="true" className="size-4 text-current" />}
+          </button>
+        </div>
+
+        {dropdownOpen && (
+          <div className="mt-2">
+            {items.map((item) => (
+              <div className="py-2 px-4" key={item.href}>
+                <React.Fragment>
+                  <Link href={item.href} className="nav-link-text">
+                    {item.text}
+                  </Link>
+                </React.Fragment>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Desktop: dropdown menu */}
+      <div className="hidden md:flex md:relative">
+        <button onClick={toggleMenu} className="nav-link-text flex flex-row justify-center gap-2">
+          {buttonText} {!dropdownOpen && <IoChevronDown className="mt-0.5" />}
+          {dropdownOpen && <IoChevronUp className="mt-0.5" />}
+        </button>
+        {dropdownOpen && (
+          <div className="bg-beige mt-2 absolute inline-block z-75 top-7 -right-2 w-54 rounded-lg shadow-lg">
+            {items.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="nav-link-text block pl-7 pr-7 py-2 text-sm text-gray-700 hover:bg-muted-blue-op-45 rounded-lg text-right"
+              >
+                {item.text}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default NavDropdown
