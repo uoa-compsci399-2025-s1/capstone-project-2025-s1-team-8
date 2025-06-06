94e70b8d93f8f2a44c5cd2a1b2e6f355d9fddda8
joohuil <99111909+joohuil@users.noreply.github.com>
2025-06-06 14:33:49 +1200

    413 frontend add download csv button for each semester (#458)
    * fix: styling of 'loading'
    
    * fix: pagination styling
    
    * fix: loading yassify
    
    * feat: added download icon to semester cards
    
    * feat: download csv functionality
    
    * fix: lint

--- src/components/Composite/ProjectCardList/ProjectCardList.tsx
+++ src/components/Composite/ProjectCardList/ProjectCardList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import type { ReactNode } from 'react'
 import ProjectCard from '@/components/Generic/ProjectCard/ProjectCard'
 import type { ProjectDetails } from '@/types/Project'
 
@@ -8,6 +9,8 @@ interface ProjectListProps {
   heading: string
   projects: ProjectDetails[]
   type?: 'student' | 'admin' | 'client'
+  icon?: ReactNode
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
 const ProjectCardList: React.FC<ProjectListProps> = ({
@@ -16,10 +19,22 @@ const ProjectCardList: React.FC<ProjectListProps> = ({
   heading,
   projects,
   type = 'admin',
+  icon,
+  onClick,
 }) => {
   return (
     <div className={`relative w-full ${className}`}>
-      <h2 className={`text-dark-blue font-inter ${headingClassName}`}>{heading}</h2>
+      <div className="flex flex-start">
+        <h2 className={`text-dark-blue font-inter ${headingClassName}`}>{heading}</h2>
+        {icon && (
+          <span
+            className="self-center pl-4 text-xl text-dark-blue hover:text-steel-blue hover:cursor-pointer"
+            onClick={onClick}
+          >
+            {icon}
+          </span>
+        )}
+      </div>
 
       <div className="flex flex-col gap-4 overflow-x-visible overflow-y-auto max-h-[490px] p-[1px] pt-0.5 pb-3">
         {projects.map((project, index) => (
