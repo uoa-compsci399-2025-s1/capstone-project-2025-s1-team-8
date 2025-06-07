a71b9a8b5f9ead0957439383a4a5d5271c554982
Eric Zheng <eric.zheng@outlook.com>
2025-06-07 15:44:42 +1200

    refactor: yassify

--- src/components/Pages/FormPage/ProtectedFormView.tsx
+++ src/components/Pages/FormPage/ProtectedFormView.tsx
@@ -5,13 +5,7 @@ import FormView from './FormView'
 import ProjectFormService from '@/lib/services/form/projectFormService'
 import { SemesterType } from '@/types/Semester'
 
-const ProtectedFormView = async ({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined }
-}): Promise<JSX.Element> => {
-  const projectId = await searchParams?.projectId
-
+const ProtectedFormView = async ({ projectId }: { projectId?: string }): Promise<JSX.Element> => {
   const { data: semesters } = await ProjectFormService.getUpcomingSemesters({
     timeframe: SemesterType.Upcoming,
     limit: 10,
