dd74a02bba4b858deb905408eaefa84d76a5a28b
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-08 21:01:18 +1200

    Add test for format date

---
+++ src/utils/date.test.ts
@@ -0,0 +1,10 @@
+import { describe } from "vitest";
+import { formatDate } from "./date";
+
+describe('Date Utils', () => {
+  it("Should format date correctly", () => {
+    const date = new Date('2023-10-01T00:00:00Z');
+    const formattedDate = formatDate(date);
+    expect(formattedDate).toBe('1/10/2023');
+  })
+});
